04b69ce86a7be6b97fee647d2de680289f110c24
Hussein Elgridly <hussein@broadinstitute.org>
2017-07-25 16:12:18 -0400

    build proper releases only on dev push

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]; then
 	sbt +publish -Dproject.isSnapshot=false
 else
 	sbt +publish -Dproject.isSnapshot=true
