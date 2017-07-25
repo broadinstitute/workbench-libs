7a8bf78776fcb5df9d556b49caf0fd7e246590ff
helgridly <helgridly@users.noreply.github.com>
2017-07-25 16:19:36 -0400

    Build -SNAP libs for anything that isn't develop (#3)

---
+++ scripts/publish.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
+	sbt +publish -Dproject.isSnapshot=false
+else
+	sbt +publish -Dproject.isSnapshot=true
+fi
