c674daf25e550a161d761f2320e091575e5df8b9
Hussein Elgridly <hussein@broadinstitute.org>
2017-07-27 00:10:50 -0400

    assertSuccessfulTries spec

--- util/src/test/scala/org/broadinstitute/dsde/workbench/util/FutureSupportSpec.scala
+++ util/src/test/scala/org/broadinstitute/dsde/workbench/util/FutureSupportSpec.scala
@@ -12,17 +12,37 @@ import FutureSupport._
 class FutureSupportSpec extends FlatSpecLike with BeforeAndAfterAll with Matchers with ScalaFutures {
   "toFutureTry" should "turn a successful Future into a successful Future(Success)" in {
     val successFuture = Future.successful(2)
-	
+    
     whenReady( toFutureTry(successFuture) ) { t =>
       t.success.value shouldBe 2
-	}
+    }
   }
   
   "toFutureTry" should "turn a failed Future into a successful Future(Failure)" in {
     val failFuture = Future.failed(new RuntimeException)
-	
+    
     whenReady( toFutureTry(failFuture) ) { t =>
       t should be a 'failure
-	}
+    }
+  }
+  
+  "assertSuccessfulTries" should "return a successful Future when given an empty map" in {
+    whenReady( assertSuccessfulTries(Map()) ) { m =>
+      m should be empty
+    }
+  }
+  
+  "assertSuccessfulTries" should "return a successful Future when all contained tries are successful" in {
+    val tries = Map( 1 -> Success(2), 2 -> Success(3) )
+    whenReady( assertSuccessfulTries(tries) ) { m =>
+      m should be Map( 1 -> 2, 2 -> 3 )
+    }
+  }
+  
+  "assertSuccessfulTries" should "return a failed Future when any contained Try is a failure" in {
+    val tries = Map( 1 -> Success(2), 2 -> Failure(new RuntimeException) )
+    whenReady( assertSuccessfulTries(tries).failed ) { f =>
+      f should be a [RuntimeException]
+    }
   }
 }
