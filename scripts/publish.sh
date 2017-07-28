7431f0a6de75554bddb9ba84f382961f6fc92917
Hussein Elgridly <hussein@broadinstitute.org>
2017-07-28 11:43:08 -0400

    more words

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-# Note we only do sbt publish here instead of sbt +publish.
-# This is because Travis runs against 2.11 and 2.12 in separate jobs, so each one publishes its version to Artifactory.
+# sbt publish publishes libs to Artifactory for the scala version sbt is running as.
+# sbt +publish publishes libs to Artifactory for all scala versions listed in crossScalaVersions.
+# We only do sbt publish here because Travis runs against 2.11 and 2.12 in separate jobs, so each one publishes its version to Artifactory.
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
 	sbt publish -Dproject.isSnapshot=false
 else
