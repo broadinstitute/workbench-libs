ec7377447dbd076addd72a5b306cba1be35b9bae
Robert Title <rtitle@broadinstitute.org>
2017-08-15 11:35:31 -0400

    Added WorkbenchInstrumented.asGaugeIfAbsent() to handle gauge name co… (#15)
    * Added WorkbenchInstrumented.asGaugeIfAbsent() to handle gauge name conflicts
    
    * Updated changelog. I'm always going to forget to do this.

--- metrics/src/main/scala/org/broadinstitute/dsde/workbench/metrics/WorkbenchInstrumented.scala
+++ metrics/src/main/scala/org/broadinstitute/dsde/workbench/metrics/WorkbenchInstrumented.scala
@@ -1,8 +1,10 @@
 package org.broadinstitute.dsde.workbench.metrics
 
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import com.codahale.metrics.{Gauge => DropwizardGauge}
 import nl.grons.metrics.scala._
 import org.broadinstitute.dsde.workbench.metrics.Expansion._
+import scala.collection.JavaConverters._
 
 /**
   * Mixin trait for instrumentation.
@@ -44,6 +46,20 @@ trait WorkbenchInstrumented extends DefaultInstrumented {
 
     def asGauge[T](name: String)(fn: => T): Gauge[T] =
       metrics.gauge(makeName(name))(fn)
+
+    def asGaugeIfAbsent[T](name: String)(fn: => T): Gauge[T] = {
+      // Get the fully qualified metric name for inspecting the registry.
+      val gaugeName = metricBaseName.append(makeName(name)).name
+      metricRegistry.getGauges().asScala.get(gaugeName) match {
+        case None =>
+          // If the gauge does not exist in the registry, create it
+          asGauge[T](name)(fn)
+        case Some(gauge) =>
+          // If the gauge exists in the registry, return it.
+          // Need to wrap the returned Java DropwizardGauge in a Scala Gauge.
+          new Gauge[T](gauge.asInstanceOf[DropwizardGauge[T]])
+      }
+    }
 
     def asTimer(name: String): Timer =
       metrics.timer(makeName(name))
