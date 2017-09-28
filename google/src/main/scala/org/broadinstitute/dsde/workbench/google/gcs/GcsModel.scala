0585533a2b67e26472d1ab747898138654f716b8
Rob Title <rtitle@broadinstitute.org>
2017-09-28 12:12:25 -0400

    Added GCS model and parsers

---
+++ google/src/main/scala/org/broadinstitute/dsde/workbench/google/gcs/GcsModel.scala
@@ -0,0 +1,137 @@
+package org.broadinstitute.dsde.workbench.google.gcs
+
+import com.google.common.net.UrlEscapers
+import java.net.URI
+import java.util.UUID
+import org.broadinstitute.dsde.workbench.google.gcs.GcsPathParser._
+import scala.util.Try
+
+/** A valid GCS bucket name */
+case class GcsBucketName(name: String) extends AnyVal
+object GcsBucketName {
+  /**
+    * Generates a unique bucket name with the given prefix. The prefix may be
+    * modified so that it adheres to bucket naming rules specified here:
+    *
+    * https://cloud.google.com/storage/docs/naming.
+    *
+    * The resulting bucket name is guaranteed to be a valid bucket name.
+    *
+    * @param prefix bucket name prefix
+    * @return generated bucket name
+    */
+  def generateUniqueBucketName(prefix: String) = {
+    // may only contain lowercase letters, numbers, underscores, dashes, or dots
+    val lowerCaseName = prefix.toLowerCase.filter { c =>
+      Character.isLetterOrDigit(c) || c == '_' || c == '-' || c == '.'
+    }
+
+    // must start with a letter or number
+    val sb = new StringBuilder(lowerCaseName)
+    if (!Character.isLetterOrDigit(sb.head)) sb.setCharAt(0, '0')
+
+    // max length of 63 chars, including the uuid
+    val uuid = UUID.randomUUID.toString
+    val maxNameLength = 63 - uuid.length - 1
+    if (sb.length > maxNameLength) sb.setLength(maxNameLength)
+
+    // must not start with "goog" or contain the string "google"
+    val processedName = sb.replaceAllLiterally("goog", "g00g")
+
+    GcsBucketName(s"$processedName-$uuid")
+  }
+}
+
+/** A GCS relative path */
+case class GcsRelativePath(name: String) extends AnyVal
+
+/** A GCS path including bucket name and path */
+case class GcsPath(bucket: GcsBucketName, path: GcsRelativePath) {
+  def toUri: String = s"$GCS_SCHEME://${bucket.name}/${path.name}"
+}
+object GcsPath {
+  def parse(str: String): Either[GcsParseError, GcsPath] =
+    GcsPathParser.parseGcsPathFromString(str)
+}
+
+case class GcsParseError(message: String) extends AnyVal
+
+private object GcsPathParser {
+  final val GCS_SCHEME = "gs"
+
+  /*
+   * Provides some level of validation of GCS bucket names.
+   * See https://cloud.google.com/storage/docs/naming for full spec
+   */
+  final val GCS_BUCKET_NAME_PATTERN =
+    """^[a-z0-9][a-z0-9-_\\.]{1,61}[a-z0-9]$""".r
+
+  /*
+   * Regex for a full GCS path which captures the bucket name.
+   */
+  final val GCS_PATH_PATTERN =
+    s"""
+      (?x)                                      # Turn on comments and whitespace insensitivity
+      ^${GCS_SCHEME}://
+      (                                         # Begin capturing group for gcs bucket name
+        [a-z0-9][a-z0-9-_\\.]{1,61}[a-z0-9]     # Regex for bucket name - soft validation, see comment above
+      )                                         # End capturing group for gcs bucket name
+      /
+      (?:
+        .*                                      # No validation here
+      )?
+    """.trim.r
+
+
+  def parseGcsPathFromString(path: String): Either[GcsParseError, GcsPath] = {
+    for {
+      uri <- parseAsUri(path)
+      _ <- validateScheme(uri)
+      host <- getAndValidateHost(path, uri)
+      relativePath <- getAndValidateRelativePath(uri)
+    } yield GcsPath(host, relativePath)
+  }
+
+  def parseAsUri(path: String): Either[GcsParseError, URI] = {
+    Try {
+      URI.create(UrlEscapers.urlFragmentEscaper().escape(path))
+    }.toEither.left.map { e =>
+      GcsParseError(s"Unparseable GCS path: ${e.getMessage}")
+    }
+  }
+
+  def validateScheme(uri: URI): Either[GcsParseError, Unit] = {
+    // Allow null or gs:// scheme
+    if (uri.getScheme == null || uri.getScheme == GCS_SCHEME) {
+      Right(())
+    } else {
+      Left(GcsParseError(s"Invalid scheme: ${uri.getScheme}"))
+    }
+  }
+
+  def getAndValidateHost(path: String, uri: URI): Either[GcsParseError, GcsBucketName] = {
+    // Get the host from the URI if we can, and validate it against GCS_BUCKET_NAME_PATTERN
+    val parsedFromUri = for {
+      h <- Option(uri.getHost)
+      _ <- GCS_BUCKET_NAME_PATTERN.findFirstMatchIn(h)
+    } yield h
+
+    // It's possible for it to not be a valid URI, but still be a valid bucket name.
+    // For example a_bucket_with_underscores is a valid GCS name but not a valid URI.
+    // So if URI parsing fails, still try to extract a valid bucket name using GCS_PATH_PATTERN.
+    val parsed = parsedFromUri.orElse {
+      for {
+        m <- GCS_PATH_PATTERN.findFirstMatchIn(path)
+        g <- Option(m.group(1))
+      } yield g
+    }
+
+    parsed.map(GcsBucketName.apply)
+      .toRight(GcsParseError(s"Could not parse bucket name from path: $path"))
+  }
+
+  def getAndValidateRelativePath(uri: URI): Either[GcsParseError, GcsRelativePath] = {
+    Option(uri.getPath).map(GcsRelativePath.apply)
+      .toRight(GcsParseError(s"Could not parse bucket relative path from path: ${uri.toString}"))
+  }
+}
---
+++ google/src/test/scala/org/broadinstitute/dsde/workbench/google/gcs/GcsModelSpec.scala
@@ -0,0 +1,56 @@
+package org.broadinstitute.dsde.workbench.google.gcs
+
+import org.scalatest.{EitherValues, FlatSpecLike, Matchers}
+
+/**
+  * Created by rtitle on 9/28/17.
+  */
+class GcsModelSpec extends FlatSpecLike with Matchers with EitherValues {
+
+  "GcsBucketName" should "generate valid names" in {
+    GcsBucketName.generateUniqueBucketName("myCluster").name should startWith ("mycluster-")
+    GcsBucketName.generateUniqueBucketName("MyCluster").name should startWith ("mycluster-")
+    GcsBucketName.generateUniqueBucketName("My_Cluster").name should startWith ("my_cluster-")
+    GcsBucketName.generateUniqueBucketName("MY_CLUSTER").name should startWith ("my_cluster-")
+    GcsBucketName.generateUniqueBucketName("MYCLUSTER").name should startWith ("mycluster-")
+    GcsBucketName.generateUniqueBucketName("_myCluster_").name should startWith ("0mycluster_-")
+    GcsBucketName.generateUniqueBucketName("my.cluster").name should startWith ("my.cluster-")
+    GcsBucketName.generateUniqueBucketName("my+cluster").name should startWith ("mycluster-")
+    GcsBucketName.generateUniqueBucketName("mY-?^&@%#@&^#cLuStEr.foo_bar").name should startWith ("my-cluster.foo_bar-")
+    GcsBucketName.generateUniqueBucketName("googMyCluster").name should startWith ("g00gmycluster-")
+    GcsBucketName.generateUniqueBucketName("my_Google_clUsTer").name should startWith("my_g00gle_cluster-")
+    GcsBucketName.generateUniqueBucketName("myClusterWhichHasAVeryLongNameBecauseIAmExtremelyVerboseInMyDescriptions").name should startWith ("myclusterwhichhasaverylong-")
+    GcsBucketName.generateUniqueBucketName("myClusterWhichHasAVeryLongNameBecauseIAmExtremelyVerboseInMyDescriptions").name.length shouldBe 63
+  }
+
+  "GcsPath" should "parse valid paths" in {
+    GcsPath.parse("gs://a-bucket/a/relative/path").right.value shouldBe GcsPath(GcsBucketName("a-bucket"), GcsRelativePath("/a/relative/path"))
+    GcsPath.parse("gs://a-123-bucket/foo/").right.value shouldBe GcsPath(GcsBucketName("a-123-bucket"), GcsRelativePath("/foo/"))
+    GcsPath.parse("gs://a-bucket-123/").right.value shouldBe GcsPath(GcsBucketName("a-bucket-123"), GcsRelativePath("/"))
+    GcsPath.parse("gs://a-bucket-123").right.value shouldBe GcsPath(GcsBucketName("a-bucket-123"), GcsRelativePath(""))
+    GcsPath.parse("//a-bucket-123/a/relative/path").right.value shouldBe GcsPath(GcsBucketName("a-bucket-123"), GcsRelativePath("/a/relative/path"))
+    GcsPath.parse("gs://a_123_bucket/a/relative/path").right.value shouldBe GcsPath(GcsBucketName("a_123_bucket"), GcsRelativePath("/a/relative/path"))
+    GcsPath.parse("gs://a.123.bucket.123/a/relative/path").right.value shouldBe GcsPath(GcsBucketName("a.123.bucket.123"), GcsRelativePath("/a/relative/path"))
+    GcsPath.parse("gs://a-bucket_123.456/a/relative/path").right.value shouldBe GcsPath(GcsBucketName("a-bucket_123.456"), GcsRelativePath("/a/relative/path"))
+    GcsPath.parse("gs://a-bucket-with-a-slightly-longer-name-42/a/relative/path").right.value shouldBe GcsPath(GcsBucketName("a-bucket-with-a-slightly-longer-name-42"), GcsRelativePath("/a/relative/path"))
+  }
+
+  it should "fail to parse invalid paths" in {
+    GcsPath.parse("foo").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("Foo/Bar").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("C:\\Windows\\System32").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://aCamelCaseBucket/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://a+bucket/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://_abucket/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://abucket-/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://.abucket./a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://a%buck%et/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://aO#&%)?et/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("mailto:mduerst@ifi.unizh.ch").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("http://a-bucket/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("ftp://a-bucket/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("file:///a-bucket/a/relative/path").left.value shouldBe a [GcsParseError]
+    GcsPath.parse("gs://a-bucket-which-has-a-very-long-name-because-i-am-extremely-verbose-in-my-descriptions-but-is-otherwise-valid/foo").left.value shouldBe a [GcsParseError]
+  }
+
+}
