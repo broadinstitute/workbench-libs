e161b6895c63c74e12f1d32d41c45acbe58d4471
dvoet <dvoet@users.noreply.github.com>
2017-11-06 08:54:37 -0500

    Workbench group identity (#37)

--- project/Settings.scala
+++ project/Settings.scala
@@ -60,7 +60,7 @@ object Settings {
   val modelSettings = commonSettings ++ List(
     name := "workbench-model",
     libraryDependencies ++= modelDependencies,
-    version := createVersion("0.5")
+    version := createVersion("0.6")
   ) ++ publishSettings
 
   val metricsSettings = commonSettings ++ List(
