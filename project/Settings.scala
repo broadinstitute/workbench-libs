7ef5108b0bdcabfe6e85eb3d1f33543afa7c326a
Rob Title <rtitle@broadinstitute.org>
2017-11-06 09:51:24 -0500

    Bump model too

--- project/Settings.scala
+++ project/Settings.scala
@@ -60,7 +60,7 @@ object Settings {
   val modelSettings = commonSettings ++ List(
     name := "workbench-model",
     libraryDependencies ++= modelDependencies,
-    version := createVersion("0.6")
+    version := createVersion("0.7")
   ) ++ publishSettings
 
   val metricsSettings = commonSettings ++ List(
