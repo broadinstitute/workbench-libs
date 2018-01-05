8607b0d03ed1b105641a7ad8bc4a6ed3bd483921
Brian Reilly <breilly@broadinstitute.org>
2018-01-05 11:52:06 -0500

    Rawls interface (#54)
    * Added Rawls client
    
    * Added logging

---
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/Rawls.scala
@@ -0,0 +1,39 @@
+package org.broadinstitute.dsde.workbench.service
+
+import com.typesafe.scalalogging.LazyLogging
+import org.broadinstitute.dsde.workbench.auth.AuthToken
+
+class Rawls(url: String) extends RestClient with LazyLogging {
+
+  object admin {
+    def deleteBillingProject(projectName: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Deleting billing project: $projectName")
+      deleteRequest(url + s"api/admin/billing/$projectName")
+    }
+  }
+
+  object workspaces {
+
+    def create(namespace: String, name: String, authDomain: Set[String] = Set.empty)
+              (implicit token: AuthToken): Unit = {
+      logger.info(s"Creating workspace: $namespace/$name authDomain: $authDomain")
+
+      val authDomainGroups = authDomain.map(a => Map("membersGroupName" -> a))
+
+      val request = Map("namespace" -> namespace, "name" -> name,
+        "attributes" -> Map.empty, "authorizationDomain" -> authDomainGroups)
+
+      postRequest(url + s"api/workspaces", request)
+    }
+
+    def delete(namespace: String, name: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Deleting workspace: $namespace/$name")
+      deleteRequest(url + s"api/workspaces/$namespace/$name")
+    }
+
+    def list()(implicit token: AuthToken):String  = {
+      logger.info(s"Listing workspaces")
+      parseResponse(getRequest(url + s"api/workspaces"))
+    }
+  }
+}
