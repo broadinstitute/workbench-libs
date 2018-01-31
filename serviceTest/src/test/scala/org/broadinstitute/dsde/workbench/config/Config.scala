89389b265055199f612adecd9100f218043544bc
gdlex4015 <garyd@broadinstitute.org>
2018-01-31 15:25:59 -0500

    adding changes to workbench-libs (#64)

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/config/Config.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/config/Config.scala
@@ -54,7 +54,7 @@ object Config extends Config {
   object Users {
     private val notSoSecretPassword = users.getString("notSoSecretPassword")
     private val userDataJson = JSON.parseFull(scala.io.Source.fromFile(users.getString("userDataPath")).getLines.mkString).get.asInstanceOf[Map[String, Map[String,String]]]
-
+    val tcgaJsonWebTokenKey = users.getString("tcgaJsonWebTokenKey")
     def makeCredsMap(jsonMap: Map[String, String]): Map[String, Credentials] = {
       for((k,v) <- jsonMap) yield (k, Credentials(v, notSoSecretPassword))
     }
@@ -96,6 +96,7 @@ object Config extends Config {
     val rawlsApiUrl: String = fireCloud.getString("rawlsApiUrl")
     val samApiUrl: String = fireCloud.getString("samApiUrl")
     val thurloeApiUrl: String = fireCloud.getString("thurloeApiUrl")
+    val tcgaAuthDomain: String = fireCloud.getString("tcgaAuthDomain")
   }
 
   object ChromeSettings {
