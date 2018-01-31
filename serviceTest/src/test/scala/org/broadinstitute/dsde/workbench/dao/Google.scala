8cbf618ac647dbd7d2f4f07172c63dd97ed0d907
Rob Title <rtitle@broadinstitute.org>
2018-01-30 20:54:26 -0500

    Get automation to compile

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
@@ -1,8 +1,10 @@
 package org.broadinstitute.dsde.workbench.dao
 
+import java.io.File
+
 import akka.actor.ActorSystem
 import org.broadinstitute.dsde.workbench.config.Config
-import org.broadinstitute.dsde.workbench.google.{HttpGoogleBigQueryDAO, HttpGoogleIamDAO, HttpGoogleStorageDAO}
+import org.broadinstitute.dsde.workbench.google.{GoogleCredentialMode, HttpGoogleBigQueryDAO, HttpGoogleIamDAO}
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
@@ -12,7 +14,9 @@ object Google {
   lazy val system = ActorSystem()
   val ec: ExecutionContextExecutor = ExecutionContext.global
 
-  lazy val googleIamDAO = new HttpGoogleIamDAO(Config.GCS.qaEmail, Config.GCS.pathToQAPem, appName, metricBaseName)(system, ec)
-  lazy val googleBigQueryDAO = new HttpGoogleBigQueryDAO(appName, metricBaseName)(system, ec)
-  lazy val googleStorageDAO = new HttpGoogleStorageDAO(Config.GCS.qaEmail, Config.GCS.pathToQAPem, appName, metricBaseName)(system, ec)
+  val pemMode = GoogleCredentialMode.Pem(Config.GCS.qaEmail, new File(Config.GCS.pathToQAPem))
+
+  lazy val googleIamDAO = new HttpGoogleIamDAO(appName, pemMode, metricBaseName)(system, ec)
+  lazy val googleBigQueryDAO = new HttpGoogleBigQueryDAO(appName, pemMode, metricBaseName)(system, ec)
+  lazy val googleStorageDAO = new HttpGoogleStorageDAO(appName, pemMode, metricBaseName)(system, ec)
 }
