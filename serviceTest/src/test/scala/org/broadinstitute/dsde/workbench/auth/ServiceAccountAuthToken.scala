3b9aed7d6ebd6de98caedd576fb847a15589a3ed
Douglas Voet <dvoet@users.noreply.github.com>
2018-02-01 10:05:40 -0500

    stop making keys

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/auth/ServiceAccountAuthToken.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/auth/ServiceAccountAuthToken.scala
@@ -13,22 +13,10 @@ import org.scalatest.time.{Seconds, Span}
 
 import scala.collection.JavaConverters._
 
-// Note: we are creating a new service account private key every time we call this case class
-
-case class ServiceAccountAuthToken(project: GoogleProject, saId: WorkbenchEmail) extends AuthToken with ScalaFutures {
+case class ServiceAccountAuthToken(privateKeyJsonString: String) extends AuthToken with ScalaFutures {
   implicit override val patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)))
 
-  // creates a new Google private key.  Be sure to call removePrivateKey() when you are done with it!
-  private lazy val serviceAccountPrivateKey: ServiceAccountKey = {
-    googleIamDAO.createServiceAccountKey(project, saId).futureValue
-  }
-
-  def removePrivateKey(): Unit = {
-    googleIamDAO.removeServiceAccountKey(project, saId, serviceAccountPrivateKey.id).futureValue
-  }
-
   override def buildCredential(): GoogleCredential = {
-    val privateKeyJsonString = serviceAccountPrivateKey.privateKeyData.decode.get
     GoogleCredential.fromStream(new ByteArrayInputStream(privateKeyJsonString.getBytes())).createScoped(authScopes.asJava)
   }
 }
