94fa3838cace8e3f838f341070aa5514afc7504b
Andrew Jang <andew7i@gmail.com>
2018-02-13 15:21:18 -0500

    remove unused import and define return type

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
@@ -5,7 +5,6 @@ import java.io.File
 import akka.actor.ActorSystem
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.Config
-import org.broadinstitute.dsde.workbench.google.GoogleCredentialModes.Token
 import org.broadinstitute.dsde.workbench.google.{GoogleCredentialModes, HttpGoogleBigQueryDAO, HttpGoogleIamDAO, HttpGoogleStorageDAO}
 import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
 
@@ -20,7 +19,7 @@ object Google {
   val pemMode = GoogleCredentialModes.Pem(WorkbenchEmail(Config.GCS.qaEmail), new File(Config.GCS.pathToQAPem))
 
   lazy val googleIamDAO = new HttpGoogleIamDAO(appName, pemMode, metricBaseName)(system, ec)
-  def googleBigQueryDAO(authToken: AuthToken) {
+  def googleBigQueryDAO(authToken: AuthToken): HttpGoogleBigQueryDAO = {
     new HttpGoogleBigQueryDAO(appName, GoogleCredentialModes.RawGoogleCredential(authToken.buildCredential()), metricBaseName)(system, ec)
   }
   lazy val googleStorageDAO = new HttpGoogleStorageDAO(appName, pemMode, metricBaseName)(system, ec)
