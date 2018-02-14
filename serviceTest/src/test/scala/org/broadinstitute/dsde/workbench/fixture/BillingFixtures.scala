734c8fd53513ec39ca5cac584d95150f312c233f
aweng98 <bweng@broadinstitute.org>
2018-02-14 18:36:16 -0500

    update withBillingProject to include optional emails

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/BillingFixtures.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/BillingFixtures.scala
@@ -2,10 +2,12 @@ package org.broadinstitute.dsde.workbench.fixture
 
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.{Config, UserPool}
+import org.broadinstitute.dsde.workbench.service.Orchestration.billing.BillingProjectRole
 import org.broadinstitute.dsde.workbench.service.Orchestration.billing.BillingProjectRole.BillingProjectRole
-import org.broadinstitute.dsde.workbench.service.{Orchestration, Rawls}
 import org.broadinstitute.dsde.workbench.service.test.CleanUp
+import org.broadinstitute.dsde.workbench.service.{Orchestration, Rawls}
 import org.scalatest.TestSuite
+
 import scala.util.Random
 
 trait BillingFixtures extends CleanUp { self: TestSuite =>
@@ -16,20 +18,29 @@ trait BillingFixtures extends CleanUp { self: TestSuite =>
     Random.alphanumeric.take(length).mkString.toLowerCase
   }
 
-  def withBillingProject(namePrefix: String)
+  def withBillingProject(namePrefix: String, memberEmails: List[String] = List())
                         (testCode: (String) => Any)(implicit token: AuthToken): Unit = {
     val billingProjectName = namePrefix + "-" + makeRandomId()
     Orchestration.billing.createBillingProject(billingProjectName, Config.Projects.billingAccountId)
+    memberEmails foreach { email =>
+      Orchestration.billing.addUserToBillingProject(billingProjectName, email, BillingProjectRole.Owner)
+    }
     try {
       testCode(billingProjectName)
     } finally {
+      memberEmails foreach { email =>
+        try {
+          Orchestration.billing.removeUserFromBillingProject(billingProjectName, email, BillingProjectRole.Owner)
+        } catch nonFatalAndLog(s"Error removing user $email from billing project $billingProjectName in withBillingProject clean-up")
+      }
       try {
         Rawls.admin.deleteBillingProject(billingProjectName)(UserPool.chooseAdmin.makeAuthToken())
       } catch nonFatalAndLog(s"Error deleting billing project in withBillingProject clean-up: $billingProjectName")
     }
   }
 
-  def addUserInBillingProject(billingProjectName: String, email: String, role: BillingProjectRole)(implicit token: AuthToken): Unit = {
+  def addUserInBillingProject(billingProjectName: String, email: String, role: BillingProjectRole)
+                             (implicit token: AuthToken): Unit = {
       Orchestration.billing.addUserToBillingProject(billingProjectName, email, role)
       register cleanUp Orchestration.billing.removeUserFromBillingProject(billingProjectName, email, role)
   }
