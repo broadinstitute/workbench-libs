8e9ac2a35947c94526f84501c2c97505fdc33a2b
Andrew <andew7i@gmail.com>
2018-02-15 10:09:41 -0500

    Google DAO takes a GoogleCredential as a passthrough (#81)

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
@@ -3,21 +3,25 @@ package org.broadinstitute.dsde.workbench.dao
 import java.io.File
 
 import akka.actor.ActorSystem
+import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.Config
 import org.broadinstitute.dsde.workbench.google.{GoogleCredentialModes, HttpGoogleBigQueryDAO, HttpGoogleIamDAO, HttpGoogleStorageDAO}
+import org.broadinstitute.dsde.workbench.google.GoogleCredentialModes.RawGoogleCredential
 import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
 object Google {
   val appName = "automation"
-  val metricBaseName = appName
+  val metricBaseName: String = appName
   lazy val system = ActorSystem()
   val ec: ExecutionContextExecutor = ExecutionContext.global
 
   val pemMode = GoogleCredentialModes.Pem(WorkbenchEmail(Config.GCS.qaEmail), new File(Config.GCS.pathToQAPem))
 
   lazy val googleIamDAO = new HttpGoogleIamDAO(appName, pemMode, metricBaseName)(system, ec)
-  lazy val googleBigQueryDAO = new HttpGoogleBigQueryDAO(appName, pemMode, metricBaseName)(system, ec)
+  def googleBigQueryDAO(authToken: AuthToken): HttpGoogleBigQueryDAO = {
+    new HttpGoogleBigQueryDAO(appName, RawGoogleCredential(authToken.buildCredential()), metricBaseName)(system, ec)
+  }
   lazy val googleStorageDAO = new HttpGoogleStorageDAO(appName, pemMode, metricBaseName)(system, ec)
 }
