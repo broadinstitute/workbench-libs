753491f3940bbbad26a4ff467b546126dcd04ca9
Joel Thibault <thibault@broadinstitute.org>
2018-02-16 15:54:04 -0500

    finishing touches

--- scripts/version_update.sh
+++ scripts/version_update.sh
@@ -18,17 +18,20 @@ HASH=$(git rev-parse HEAD)
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+# ignore failure if the remote does not exist
 git remote rm ${REMOTE_NAME} || true
 git remote add ${REMOTE_NAME} https://${GH_TOKEN}@github.com/broadinstitute/workbench-libs.git > /dev/null 2>&1
 git fetch ${REMOTE_NAME}
 git checkout ${REMOTE_NAME}/${PUSH_BRANCH}
 
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
-
+# Don't run twice: prevent Scala 2.11 runs
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_SCALA_VERSION" == "2.12" ]]; then
   for f in README.md */CHANGELOG.md
     do
       echo "Replacing ${TO_REPLACE} with ${HASH} in ${f}..."
       sed -i "s/${TO_REPLACE}/${HASH}/g" ${f}
+
+      # will do nothing if there's nothing to add
       git add ${f}
 
       # will fail if no change: ignore it
@@ -37,5 +40,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; the
 
   # push one commit per update.  Will fail if no changes: ignore it
   git push ${REMOTE_NAME} HEAD:${PUSH_BRANCH} || true
+
+  git remote rm ${REMOTE_NAME}
   git checkout -
 fi
