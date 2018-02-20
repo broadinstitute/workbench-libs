6906698e8df39050f29a7fa2b2481c57dd9728f6
Joel Thibault <thibault@broadinstitute.org>
2018-02-20 11:15:15 -0500

    Test fixes - only rm remote on actual run - truncate hash at 7 chars

--- scripts/version_update.sh
+++ scripts/version_update.sh
@@ -13,19 +13,21 @@ REMOTE_NAME="token"
 # text string target for auto-replacement
 TO_REPLACE="TRAVIS-REPLACE-ME"
 
-HASH=$(git rev-parse HEAD)
+HASH=$(git rev-parse HEAD | cut -c -7)
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-# ignore failure if the remote does not exist
-git remote rm ${REMOTE_NAME} || true
-git remote add ${REMOTE_NAME} https://${GH_TOKEN}@github.com/broadinstitute/workbench-libs.git > /dev/null 2>&1
-git fetch ${REMOTE_NAME}
-git checkout ${REMOTE_NAME}/${PUSH_BRANCH}
-
 # Don't run twice: prevent Scala 2.11 runs
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_SCALA_VERSION" == "2.12" ]]; then
+  echo "Auto-updating version hashes..."
+
+  # ignore failure if the remote does not exist
+  git remote rm ${REMOTE_NAME} || true
+  git remote add ${REMOTE_NAME} https://${GH_TOKEN}@github.com/broadinstitute/workbench-libs.git > /dev/null 2>&1
+  git fetch ${REMOTE_NAME}
+  git checkout ${REMOTE_NAME}/${PUSH_BRANCH}
+
   for f in README.md */CHANGELOG.md
     do
       echo "Replacing ${TO_REPLACE} with ${HASH} in ${f}..."
@@ -43,4 +45,6 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" && "$TR
 
   git remote rm ${REMOTE_NAME}
   git checkout -
+
+  echo "Finished updating version hashes."
 fi
