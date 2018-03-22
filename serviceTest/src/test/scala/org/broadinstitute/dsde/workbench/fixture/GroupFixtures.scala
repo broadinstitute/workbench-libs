c7e4984e1f70162ef03c0761a60b59e229956fc9
jmthibault79 <thibault@broadinstitute.org>
2018-03-22 16:53:37 -0400

    Fixtures no longer require WebBrowserSpec (#99)

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/GroupFixtures.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/GroupFixtures.scala
@@ -4,15 +4,14 @@ import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.service.Orchestration.groups.GroupRole
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config._
-import org.broadinstitute.dsde.workbench.service.test.CleanUp
-import org.broadinstitute.dsde.workbench.service.test.WebBrowserSpec
-import org.broadinstitute.dsde.workbench.service.util.Util
+import org.broadinstitute.dsde.workbench.service.Orchestration
+import org.broadinstitute.dsde.workbench.service.util.{ExceptionHandling, Util}
 import org.scalatest.TestSuite
 
 /**
   * Fixtures for creating and cleaning up test groups.
   */
-trait GroupFixtures extends CleanUp with LazyLogging { self: WebBrowserSpec with TestSuite =>
+trait GroupFixtures extends ExceptionHandling with LazyLogging { self: TestSuite =>
 
   def groupNameToEmail(groupName: String): String = s"GROUP_$groupName@${Config.GCS.appsDomain}"
 
@@ -22,18 +21,18 @@ trait GroupFixtures extends CleanUp with LazyLogging { self: WebBrowserSpec with
     val groupName = Util.appendUnderscore(namePrefix) + Util.makeUuid
 
     try {
-      api.groups.create(groupName)
+      Orchestration.groups.create(groupName)
       memberEmails foreach { email =>
-        api.groups.addUserToGroup(groupName, email, GroupRole.Member)
+        Orchestration.groups.addUserToGroup(groupName, email, GroupRole.Member)
       }
 
       testCode(groupName)
 
     } finally {
       memberEmails foreach { email =>
-        api.groups.removeUserFromGroup(groupName, email, GroupRole.Member)
+        Orchestration.groups.removeUserFromGroup(groupName, email, GroupRole.Member)
       }
-      api.groups.delete(groupName)
+      Orchestration.groups.delete(groupName)
     }
   }
 }
