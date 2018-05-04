4a11fa08223e57059d6da660996acbff134a90b4
aweng98 <bweng@broadinstitute.org>
2018-05-04 10:52:28 -0400

    modify Credential class

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/config/Credentials.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/config/Credentials.scala
@@ -1,7 +1,23 @@
 package org.broadinstitute.dsde.workbench.config
 
+import java.util.concurrent.TimeUnit
+
+import com.google.common.cache.{CacheBuilder, CacheLoader}
 import org.broadinstitute.dsde.workbench.auth.{AuthToken, UserAuthToken}
 
-case class Credentials (email: String, password: String) {
-  def makeAuthToken(): AuthToken = UserAuthToken(this)
+
+case class Credentials (val email: String, val password: String) {
+  def makeAuthToken(): AuthToken = Credentials.cache.get(this)
+}
+
+object Credentials {
+  val cache = CacheBuilder.newBuilder()
+    .expireAfterWrite(3600, TimeUnit.SECONDS)
+    .build(
+      new CacheLoader[Credentials, AuthToken] {
+        def load(credentials: Credentials): AuthToken = {
+          UserAuthToken(credentials)
+        }
+      }
+    )
 }
