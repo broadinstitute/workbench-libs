44dccc96f136d8ae294fde32c91035165fabf396
jmthibault79 <thibault@broadinstitute.org>
2018-05-09 10:19:22 -0400

    Add SubWorkflowFixtures and refactor RandomUtil (#117)

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/MethodFixtures.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/MethodFixtures.scala
@@ -4,18 +4,17 @@ import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.service.Orchestration
 import org.broadinstitute.dsde.workbench.service.test.RandomUtil
 import org.broadinstitute.dsde.workbench.service.util.ExceptionHandling
-import org.broadinstitute.dsde.workbench.service.util.Util.{appendUnderscore, makeUuid}
 import org.scalatest.TestSuite
 
 trait MethodFixtures extends ExceptionHandling with RandomUtil { self: TestSuite =>
 
-  def withMethod(testName:String, method:Method, numSnapshots: Int = 1, cleanUp: Boolean = true)
+  def withMethod(testName: String, method: Method, numSnapshots: Int = 1, cleanUp: Boolean = true)
                 (testCode: (String) => Any)
                 (implicit token: AuthToken): Unit = {
     // create a method
-    val methodName: String = appendUnderscore(testName) + makeUuid
+    val methodName: String = uuidWithPrefix(testName)
     for (i <- 1 to numSnapshots)
-      Orchestration.methods.createMethod(method.creationAttributes + ("name"->methodName))
+      Orchestration.methods.createMethod(method.creationAttributes + ("name" -> methodName))
     try {
       testCode(methodName)
     } catch {
@@ -33,10 +32,10 @@ trait MethodFixtures extends ExceptionHandling with RandomUtil { self: TestSuite
 
   }
 
-  def withMethod(methodName: String)
+  def withMethod(prefix: String)
                 (testCode: ((String, String)) => Any)
                 (implicit token: AuthToken): Unit = {
-    val name = methodName + randomUuid
+    val name = uuidWithPrefix(prefix)
     val attributes = MethodData.SimpleMethod.creationAttributes + ("name" -> name)
     val namespace = attributes("namespace")
     Orchestration.methods.createMethod(attributes)
@@ -47,9 +46,6 @@ trait MethodFixtures extends ExceptionHandling with RandomUtil { self: TestSuite
       case t: Exception =>
         logger.error("MethodFixtures.withMethod Exception: ", t)
         throw t // end test execution
-    } finally {
-
     }
-
   }
 }
