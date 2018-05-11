3a30fe4301925afd074345559ae7c08d825c95fb
jbuabbud <jbuabbud@broadinstitute.org>
2018-05-11 13:30:47 -0400

    comment

--- google/src/main/scala/org/broadinstitute/dsde/workbench/google/HttpGoogleBigQueryDAO.scala
+++ google/src/main/scala/org/broadinstitute/dsde/workbench/google/HttpGoogleBigQueryDAO.scala
@@ -48,6 +48,7 @@ class HttpGoogleBigQueryDAO(appName: String,
     val job = new Job()
       .setConfiguration(new JobConfiguration()
         .setQuery(new JobConfigurationQuery()
+          // This defaults to true in current version. Standard SQL is required for query parameters.
           .setUseLegacySql(java.lang.Boolean.FALSE)
           .setParameterMode(parameterMode)
           .setQueryParameters(queryParameters.asJava)
