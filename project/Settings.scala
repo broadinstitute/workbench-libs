ca7ef741ab4417be8bc924d948954fa9ed36fb20
Joel Thibault <thibault@broadinstitute.org>
2018-05-17 13:11:26 -0400

    Bump service tests to 0.9 due to API change, and identify last 0.8 version

--- project/Settings.scala
+++ project/Settings.scala
@@ -79,7 +79,7 @@ object Settings {
   val serviceTestSettings = commonSettings ++ List(
     name := "workbench-service-test",
     libraryDependencies ++= serviceTestDependencies,
-    version := createVersion("0.8")
+    version := createVersion("0.9")
   ) ++ publishSettings
 
   val notificationsSettings = commonSettings ++ List(
