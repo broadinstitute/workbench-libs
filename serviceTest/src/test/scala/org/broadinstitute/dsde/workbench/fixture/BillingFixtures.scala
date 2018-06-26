9fe9b3a1f2b38e795cbffabc215f6f07239f4122
jbuabbud <jbuabbud@broadinstitute.org>
2018-06-26 11:36:26 -0400

    Created BillingFixtures functions that accept AuthToken instead of Credentials so that billing projects can be claimed by service accounts

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/BillingFixtures.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/BillingFixtures.scala
@@ -42,12 +42,21 @@ trait BillingFixtures extends ExceptionHandling with LazyLogging with CleanUp wi
   }
 
   case class ClaimedProject(projectName: String, gpAlloced: Boolean) {
+    @deprecated(message = "cleanup with Credentials is deprecated. Use cleanup with AuthToken and email", since = "workbench-service-test-0.12")
     def cleanup(ownerCreds: Credentials): Unit = {
+      val ownerToken = ownerCreds.makeAuthToken()
       if (gpAlloced)
-        releaseGPAllocProject(projectName, ownerCreds)
+        releaseGPAllocProject(projectName, ownerToken, ownerCreds.email)
       else {
-        deleteBillingProject(projectName)(ownerCreds.makeAuthToken())
+        deleteBillingProject(projectName)(ownerToken)
       }
+    }
+
+    def cleanup(ownerToken: AuthToken, ownerEmail: String): Unit = {
+        if (gpAlloced)
+        releaseGPAllocProject(projectName, ownerToken, ownerEmail)
+      else
+        deleteBillingProject(projectName)(ownerToken)
     }
   }
 
@@ -97,6 +106,7 @@ trait BillingFixtures extends ExceptionHandling with LazyLogging with CleanUp wi
     * @param userEmails a List of emails (as Strings) to add as users of this project
     * @return Some(GPAllocProject) if it succeeded, None if it did not
     */
+  @deprecated(message = "claimGPAllocProject with Credentials is deprecated. Use claimGPAllocProject with AuthToken", since = "workbench-service-test-0.12")
   def claimGPAllocProject(newOwnerCreds: Credentials, ownerEmails: List[String] = List(), userEmails: List[String] = List()): ClaimedProject = {
     //request a GPAlloced project as the potential new owner
     val newOwnerToken = newOwnerCreds.makeAuthToken()
@@ -120,12 +130,45 @@ trait BillingFixtures extends ExceptionHandling with LazyLogging with CleanUp wi
   }
 
   /**
+    * Manually claim a project provisioned by GPAlloc and optionall add members.
+    * As opposed to `Credentials`, accepts `AuthToken` and `String` values for the new owner.
+    * This way a GPAlloc project can be claimed as a pet SA.
+    *
+    * @param newOwnerToken AuthToken for the new billing project owner to pass to billing project endpoints
+    * @param newOwnerEmail Email for the new billing project owner
+    * @param ownerEmails a List of emails (as Strings) to add as owners of this project
+    * @param userEmails a List of emails (as Strings) to add as users of this project
+    * @return Some(GPAllocProject) if it succeeded, none if it failed
+    */
+  def claimGPAllocProject(newOwnerToken: AuthToken, newOwnerEmail: String, ownerEmails: List[String] = List(), userEmails: List[String] = List()): ClaimedProject = {
+    //request a GPAlloced project as the potential new owner
+    GPAlloc.projects.requestProject(newOwnerToken) match {
+      case Some(project) =>
+        //the Rawls endpoint to register a precreated project needs to be called by a Rawls admin
+        //but it also takes the new owner's UserInfo in order to create the resource as them in Sam
+        val adminToken = UserPool.chooseAdmin.makeAuthToken()
+        val newOwnerUserInfo = UserInfo(OAuth2BearerToken(newOwnerToken.value), WorkbenchUserId("0"), WorkbenchEmail(newOwnerEmail), 3600)
+        Rawls.admin.claimProject(project.projectName, project.cromwellAuthBucketUrl, newOwnerUserInfo)(adminToken)
+
+        addMembersToBillingProject(project.projectName, ownerEmails, BillingProjectRole.Owner)(newOwnerToken)
+        addMembersToBillingProject(project.projectName, userEmails, BillingProjectRole.User)(newOwnerToken)
+
+        ClaimedProject(project.projectName, gpAlloced = true)
+      case _ =>
+        logger.warn("claimGPAllocProject got no project back from GPAlloc. Falling back to making a brand new one...")
+        val billingProjectName = createNewBillingProject("billingproj", ownerEmails, userEmails)(newOwnerToken)
+        ClaimedProject(billingProjectName, gpAlloced = false)
+    }
+  }
+
+  /**
     * Release a billing project back to GPAlloc when you are done with it.
     * Consider using `withCleanBillingProject()` instead if you don't need to control the use of projects.
     *
     * @param projectName the GPAllocProject to release
     * @param ownerCreds the Credentials of the current owner of the project
     */
+  @deprecated(message = "releaseGPAllocProject with Credentials is deprecated. Use releaseGPAllocProject with AuthToken and email", since = "workbench-service-test-0.12")
   def releaseGPAllocProject(projectName: String, ownerCreds: Credentials): Unit = {
     val ownerToken = ownerCreds.makeAuthToken()
     val adminToken = UserPool.chooseAdmin.makeAuthToken()
@@ -133,6 +176,21 @@ trait BillingFixtures extends ExceptionHandling with LazyLogging with CleanUp wi
 
     Rawls.admin.releaseProject(projectName, newOwnerUserInfo)(adminToken)
 
+    GPAlloc.projects.releaseProject(projectName)(ownerToken)
+  }
+
+  /**
+    * Release a billing project back to GPAlloc when you are done with it.
+    * Consider using `withCleanBillingProject()` instead if you don't need to control the use of projects.
+    *
+    * @param projectName the GPAllocProject to release
+    * @param ownerToken the AuthToken of the current owner of the project
+    * @param ownerEmail the email string of the current owner
+    */
+  def releaseGPAllocProject(projectName: String, ownerToken: AuthToken, ownerEmail: String): Unit = {
+    val adminToken = UserPool.chooseAdmin.makeAuthToken()
+    val newOwnerUserInfo = UserInfo(OAuth2BearerToken(ownerToken.value), WorkbenchUserId("0"), WorkbenchEmail(ownerEmail), 3600)
+    Rawls.admin.releaseProject(projectName, newOwnerUserInfo)(adminToken)
     GPAlloc.projects.releaseProject(projectName)(ownerToken)
   }
 
@@ -145,13 +203,36 @@ trait BillingFixtures extends ExceptionHandling with LazyLogging with CleanUp wi
     * @param userEmails a List of emails (as Strings) to add as users of this project
     * @param testCode your test
     */
+  @deprecated(message = "withCleanBillingProject with Credentials is deprecated. Use withCleanBillingProject with AuthToken and email", since = "workbench-service-test-0.12")
   def withCleanBillingProject(newOwnerCreds: Credentials, ownerEmails: List[String] = List(), userEmails: List[String] = List())(testCode: (String) => Any): Unit = {
     val project = claimGPAllocProject(newOwnerCreds, ownerEmails, userEmails)
     val testTrial = Try {
       testCode(project.projectName)
     }
     val cleanupTrial = Try {
-      project.cleanup(newOwnerCreds)
+      project.cleanup(newOwnerCreds.makeAuthToken(), newOwnerCreds.email)
+    }
+
+    CleanUp.runCodeWithCleanup(testTrial, cleanupTrial)
+  }
+
+  /**
+    * Use a billing project provided by GPAlloc for the purpose of running tests against it.  This method will claim
+    * * a project for the duration of the test and release it when the test is done.
+    *
+    * @param newOwnerToken The AuthToken for the new billing project owner to pass to billing project endpoints
+    * @param newOwnerEmail The email of the new billing project owner
+    * @param ownerEmails a List of emails (as Strings) to add as owners of this project
+    * @param userEmails a List of emails (as Strings) to add as users of this project
+    * @param testCode your test
+    */
+  def withCleanBillingProject(newOwnerToken: AuthToken, newOwnerEmail: String, ownerEmails: List[String] = List(), userEmails: List[String] = List())(testCode: (String) => Any): Unit = {
+    val project = claimGPAllocProject(newOwnerToken, newOwnerEmail, ownerEmails, userEmails)
+    val testTrial = Try {
+      testCode(project.projectName)
+    }
+    val cleanupTrial = Try {
+      project.cleanup(newOwnerToken, newOwnerEmail)
     }
 
     CleanUp.runCodeWithCleanup(testTrial, cleanupTrial)
