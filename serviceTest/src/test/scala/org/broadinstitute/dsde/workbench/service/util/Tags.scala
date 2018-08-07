3988d0a2f2d26e06288aa06f31f2451c158dfd7e
aweng98 <35533885+aweng98@users.noreply.github.com>
2018-08-07 13:51:42 -0400

    Tags (#154)

---
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/util/Tags.scala
@@ -0,0 +1,9 @@
+package org.broadinstitute.dsde.workbench.service.util
+
+import org.scalatest.Tag
+
+object Tags {
+  object ProdTest extends Tag("ProdTest")
+  object SmokeTest extends Tag("SmokeTest")
+  object SignInRealTest extends Tag("SignInRealTest")
+}
