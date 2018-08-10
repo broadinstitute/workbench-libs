7ee5eb897f8420fec2d324796b193c0913866983
Qi Wang <qi@broadinstitute.org>
2018-08-10 15:09:39 -0400

    add googleSubjectId as a separate field

--- model/src/main/scala/org/broadinstitute/dsde/workbench/model/UserInfo.scala
+++ model/src/main/scala/org/broadinstitute/dsde/workbench/model/UserInfo.scala
@@ -2,4 +2,4 @@ package org.broadinstitute.dsde.workbench.model
 
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
 
-case class UserInfo(accessToken: OAuth2BearerToken, userId: WorkbenchUserId, userEmail: WorkbenchEmail, tokenExpiresIn: Long)
+case class UserInfo(accessToken: OAuth2BearerToken, user: WorkbenchUser, tokenExpiresIn: Long)
