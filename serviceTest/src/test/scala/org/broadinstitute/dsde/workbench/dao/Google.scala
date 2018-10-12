01ca8e93c8b6d85ed05b4594d394c423db46a66f
Marc Talbott <mtalbott@broadinstitute.org>
2018-10-12 15:26:30 -0400

    add sam functions for use in google sync automation test (#170)
    * add functions for use in google group sync automation test
    
    * add deleteGroup function for cleanup
    
    * change parameters to accept string instead of WorkbenchEmail
    
    * add service account user to pemMode
    
    * change qa email to qa client id
    
    * try to use sam service account
    
    * remove 'dev.' from sub email
    
    * revert client id to appropriate one
    
    * revert to qa email over client id
    
    * add list resource policies function, remove unused sync function
    
    * change list resource policies return type
    
    * update readme and changelog to indicate replacement; add different pemMode to remove possible changes to behavior for other Google DAOs
    
    * address comments; mostly rename stuff and also remove an unused import
    
    * update missed variable name change

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
@@ -5,7 +5,7 @@ import java.io.File
 import akka.actor.ActorSystem
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.ServiceTestConfig
-import org.broadinstitute.dsde.workbench.google.{GoogleCredentialModes, HttpGoogleBigQueryDAO, HttpGoogleIamDAO, HttpGoogleStorageDAO}
+import org.broadinstitute.dsde.workbench.google._
 import org.broadinstitute.dsde.workbench.google.GoogleCredentialModes.RawGoogleCredential
 import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
 
@@ -18,10 +18,12 @@ object Google {
   val ec: ExecutionContextExecutor = ExecutionContext.global
 
   val pemMode = GoogleCredentialModes.Pem(WorkbenchEmail(ServiceTestConfig.GCS.qaEmail), new File(ServiceTestConfig.GCS.pathToQAPem))
+  val pemModeWithServiceAccountUser = GoogleCredentialModes.Pem(WorkbenchEmail(ServiceTestConfig.GCS.qaEmail), new File(ServiceTestConfig.GCS.pathToQAPem), Option(WorkbenchEmail("google@dev.test.firecloud.org")))
 
   lazy val googleIamDAO = new HttpGoogleIamDAO(appName, pemMode, metricBaseName)(system, ec)
   def googleBigQueryDAO(authToken: AuthToken): HttpGoogleBigQueryDAO = {
     new HttpGoogleBigQueryDAO(appName, RawGoogleCredential(authToken.buildCredential()), metricBaseName)(system, ec)
   }
   lazy val googleStorageDAO = new HttpGoogleStorageDAO(appName, pemMode, metricBaseName)(system, ec)
+  lazy val googleDirectoryDAO = new HttpGoogleDirectoryDAO(appName, pemModeWithServiceAccountUser, metricBaseName)(system, ec)
 }
--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/Sam.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/Sam.scala
@@ -4,7 +4,6 @@ import akka.http.scaladsl.model.StatusCodes
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.{UserPool, _}
-import org.broadinstitute.dsde.workbench.dao.Google.googleIamDAO
 import org.broadinstitute.dsde.workbench.model.{WorkbenchEmail, WorkbenchUserId}
 import org.broadinstitute.dsde.workbench.model.google.{GoogleProject, ServiceAccountKey, ServiceAccountName}
 import org.broadinstitute.dsde.workbench.service.Sam.user
@@ -98,6 +97,41 @@ object Sam extends Sam {
     def deletePetServiceAccountKey(project: String, keyId: String)(implicit token: AuthToken): Unit = {
       logger.info(s"Deleting pet service account key $keyId in project $project")
       deleteRequest(url + s"api/google/user/petServiceAccount/$project/key/$keyId")
+    }
+
+    def createGroup(groupName: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Creating managed group with id: $groupName")
+      postRequest(url + s"api/groups/v1/$groupName")
+    }
+
+    def deleteGroup(groupName: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Deleting managed group with id: $groupName")
+      deleteRequest(url + s"api/groups/v1/$groupName")
+    }
+
+    def syncPolicy(resourceTypeName: String, resourceId: String, policyName: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Syncing $policyName in $resourceId of type $resourceTypeName")
+      postRequest(url + s"api/google/v1/resource/$resourceTypeName/$resourceId/$policyName/sync")
+    }
+
+    def addUserToPolicy(groupName: String, policyName: String, memberEmail: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Adding $memberEmail to $policyName in $groupName")
+      putRequest(url + s"api/groups/v1/$groupName/$policyName/$memberEmail")
+    }
+
+    def removeUserFromPolicy(groupName: String, policyName: String, memberEmail: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Removing $memberEmail from $policyName in $groupName")
+      deleteRequest(url + s"api/groups/v1/$groupName/$policyName/$memberEmail")
+    }
+
+    def listResourcePolicies(resourceTypeName: String, resourceId: String)(implicit token: AuthToken): Set[String] = {
+      logger.info(s"Listing policies for $resourceId")
+      parseResponseAs[Set[String]](getRequest(url + s"api/resources/v1/$resourceTypeName/$resourceId/policies"))
+    }
+
+    def setPolicyMembers(groupName: String, policyName: String, memberEmails: Set[String])(implicit token: AuthToken): Unit = {
+      logger.info(s"Overwriting members in policy $policyName of $groupName")
+      putRequest(url + s"api/groups/v1/$groupName/$policyName", memberEmails)
     }
   }
 
