00170b21e5ce62556e9b2e933b208801aecf0498
Hussein Elgridly <hussein@broadinstitute.org>
2018-10-23 16:44:36 -0400

    add get wsid

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/Rawls.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/Rawls.scala
@@ -54,6 +54,11 @@ trait Rawls extends RestClient with LazyLogging {
       mapper.readTree(response).at("/workspace/bucketName").asText()
     }
 
+    def getWorkspaceId(namespace: String, name: String)(implicit token: AuthToken): String = {
+      val response = parseResponse(getRequest(url + s"api/workspaces/$namespace/$name"))
+      mapper.readTree(response).at("/workspace/workspaceId").asText()
+    }
+
     def list()(implicit token: AuthToken):String  = {
       logger.info(s"Listing workspaces")
       parseResponse(getRequest(url + s"api/workspaces"))
