f074409e836d65972b6adeccff53f2faf435db4c
Marc Talbott <mtalbott@broadinstitute.org>
2019-03-05 10:22:54 -0500

    Add KMS interpreter and service (#202)
    * initial effort at kms service and interpreter
    
    * get it to compile
    
    * add object w/ apply
    
    * remove unused import
    
    * add object for building the client with passed in service account credentials
    
    * remove unused import
    
    * make two of the functions private and add test println
    
    * add mock kms interpreter
    
    * address pr comments
    
    * remove option type for getIamPolicy because if there is no policy set it just returns an empty policy anyways
    
    * make getIamPolicy private again bc no longer needed for testing
    
    * add optional key rotation
    
    * add travis-replace-me

---
+++ google/src/main/scala/org/broadinstitute/dsde/workbench/google/GoogleKmsInterpreter.scala
@@ -0,0 +1,131 @@
+package org.broadinstitute.dsde.workbench.google
+
+import cats.implicits._
+import cats.effect.{ContextShift, Resource, Sync}
+import com.google.api.gax.core.FixedCredentialsProvider
+import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.kms.v1.CryptoKey.CryptoKeyPurpose
+import com.google.cloud.kms.v1._
+import com.google.iam.v1.{Binding, Policy}
+import com.google.protobuf.{Timestamp, Duration}
+import org.broadinstitute.dsde.workbench.model.google.GoogleProject
+
+import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext
+import scala.language.higherKinds
+
+/**
+  * Interpreter for Google KMS (Key Management Service) algebra
+  *
+  * created by mtalbott on 1/18/19
+  */
+
+private[google] class GoogleKmsInterpreter[F[_]: Sync: ContextShift](client: KeyManagementServiceClient, blockingEc: ExecutionContext) extends GoogleKmsService[F] {
+  override def createKeyRing(project: GoogleProject, location: Location, keyRingId: KeyRingId): F[KeyRing] = {
+    val locationName = LocationName.format(project.value, location.value)
+    blockingF(Sync[F].delay[KeyRing] {
+      client.createKeyRing(locationName, keyRingId.value, KeyRing.newBuilder().build())
+    })
+  }
+
+  override def getKeyRing(project: GoogleProject, location: Location, keyRingId: KeyRingId): F[Option[KeyRing]] = {
+    val keyRingName = KeyRingName.format(project.value, location.value, keyRingId.value)
+    blockingF(Sync[F].delay[Option[KeyRing]] {
+      Option(client.getKeyRing(keyRingName))
+    })
+  }
+
+  override def createKey(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId, nextRotationTimeOpt: Option[Timestamp] = None, rotationPeriodOpt: Option[Duration] = None): F[CryptoKey] = {
+    val keyRingName = KeyRingName.format(project.value, location.value, keyRingId.value)
+    (nextRotationTimeOpt, rotationPeriodOpt) match {
+      case (Some(nextRotationTime), Some(rotationPeriod)) => blockingF(Sync[F].delay[CryptoKey] {
+        client.createCryptoKey(keyRingName, keyId.value, CryptoKey.newBuilder()
+          .setPurpose(CryptoKeyPurpose.ENCRYPT_DECRYPT)
+          .setNextRotationTime(nextRotationTime)
+          .setRotationPeriod(rotationPeriod)
+          .build())
+      })
+      case (_, _) => blockingF(Sync[F].delay[CryptoKey] {
+        client.createCryptoKey(keyRingName, keyId.value, CryptoKey.newBuilder()
+          .setPurpose(CryptoKeyPurpose.ENCRYPT_DECRYPT)
+          .build())
+      })
+    }
+  }
+
+  override def getKey(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId): F[Option[CryptoKey]] = {
+    val keyName = CryptoKeyName.format(project.value, location.value, keyRingId.value, keyId.value)
+    blockingF(Sync[F].delay[Option[CryptoKey]] {
+      Option(client.getCryptoKey(keyName))
+    })
+  }
+
+  override def addMemberToKeyPolicy(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId, member: String, role: String): F[Policy] = {
+    for {
+      currentIamPolicy <- getIamPolicy(project, location, keyRingId, keyId)
+
+      newBinding = Binding.newBuilder()
+        .setRole(role)
+        .addMembers(member)
+        .build()
+      newPolicy = Policy.newBuilder(currentIamPolicy)
+        .addBindings(newBinding)
+        .build()
+
+      _ <- setIamPolicy(project, location, keyRingId, keyId, newPolicy)
+    } yield newPolicy
+  }
+
+  override def removeMemberFromKeyPolicy(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId, member: String, role: String): F[Policy] = {
+    for {
+      currentIamPolicy <- getIamPolicy(project, location, keyRingId, keyId)
+
+      otherBindings = currentIamPolicy.getBindingsList.asScala.toList.filter(binding => !binding.getRole.equals(role))
+      newBindings = currentIamPolicy.getBindingsList.asScala.toList.filter(binding => binding.getRole.equals(role)).map { binding =>
+        Binding.newBuilder().setRole(binding.getRole)
+          .addAllMembers(binding.getMembersList.asScala.filter(!_.equals(member)).asJava)
+          .build()
+      }
+
+      newPolicy = Policy.newBuilder()
+        .addAllBindings((otherBindings ++ newBindings).asJava)
+        .build()
+
+      _ <- setIamPolicy(project, location, keyRingId, keyId, newPolicy)
+    } yield newPolicy
+  }
+
+  private def getIamPolicy(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId): F[Policy] = {
+    val keyName = CryptoKeyName.format(project.value, location.value, keyRingId.value, keyId.value)
+    blockingF(Sync[F].delay[Policy] {
+      client.getIamPolicy(keyName)
+    })
+  }
+
+  private def setIamPolicy(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId, newPolicy: Policy): F[Policy] = {
+    val keyName = CryptoKeyName.format(project.value, location.value, keyRingId.value, keyId.value)
+    blockingF(Sync[F].delay[Policy] {
+      client.setIamPolicy(keyName, newPolicy)
+    })
+  }
+
+  private def blockingF[A](fa: F[A]): F[A] = ContextShift[F].evalOn(blockingEc)(fa)
+}
+
+object GoogleKmsInterpreter {
+  def apply[F[_]: Sync: ContextShift](client: KeyManagementServiceClient, blockingEc: ExecutionContext): GoogleKmsInterpreter[F] = new GoogleKmsInterpreter[F](client, blockingEc)
+
+  def client[F[_]: Sync](pathToJson: String): Resource[F, KeyManagementServiceClient] =
+    for {
+      credentials <- org.broadinstitute.dsde.workbench.util.readFile(pathToJson)
+      client <- Resource.make[F, KeyManagementServiceClient](
+        Sync[F].delay(
+          KeyManagementServiceClient.create(
+            KeyManagementServiceSettings.newBuilder()
+                .setCredentialsProvider(FixedCredentialsProvider.create(
+                  ServiceAccountCredentials.fromStream(credentials)))
+                .build())
+        )
+      )(client => Sync[F].delay(client.close()))
+    } yield client
+}
---
+++ google/src/test/scala/org/broadinstitute/dsde/workbench/google/mock/FakeGoogleKmsInterpreter.scala
@@ -0,0 +1,23 @@
+package org.broadinstitute.dsde.workbench.google.mock
+
+import cats.effect._
+import com.google.cloud.kms.v1.{CryptoKey, KeyRing}
+import com.google.iam.v1.Policy
+import com.google.protobuf.{Duration, Timestamp}
+import org.broadinstitute.dsde.workbench.google.{GoogleKmsService, KeyId, KeyRingId, Location}
+import org.broadinstitute.dsde.workbench.model.google.GoogleProject
+
+/**
+  * Fake Google Key Management Service Interpreter
+  *
+  * created by mtalbott 1/28/19
+  */
+
+object FakeGoogleKmsInterpreter extends GoogleKmsService[IO] {
+  override def createKeyRing(project: GoogleProject, location: Location, keyRingId: KeyRingId): IO[KeyRing] = IO.pure(KeyRing.newBuilder().build())
+  override def getKeyRing(project: GoogleProject, location: Location, keyRingId: KeyRingId): IO[Option[KeyRing]] = ???
+  override def createKey(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId, nextRotationTimeOpt: Option[Timestamp], rotationPeriodOpt: Option[Duration]): IO[CryptoKey] = IO.pure(CryptoKey.newBuilder().build())
+  override def getKey(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId): IO[Option[CryptoKey]] = ???
+  override def addMemberToKeyPolicy(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId, member: String, role: String): IO[Policy] = IO.pure(Policy.newBuilder().build())
+  override def removeMemberFromKeyPolicy(project: GoogleProject, location: Location, keyRingId: KeyRingId, keyId: KeyId, member: String, role: String): IO[Policy] = ???
+}
