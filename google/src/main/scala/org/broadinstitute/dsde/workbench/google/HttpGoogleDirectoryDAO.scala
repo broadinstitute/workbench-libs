4a7367ca9afc4076871b9a02ef8f7a0e321b315f
Douglas Voet <dvoet@users.noreply.github.com>
2019-04-01 15:49:50 -0400

    handle 400 error removing user that does not exist from group

--- google/src/main/scala/org/broadinstitute/dsde/workbench/google/HttpGoogleDirectoryDAO.scala
+++ google/src/main/scala/org/broadinstitute/dsde/workbench/google/HttpGoogleDirectoryDAO.scala
@@ -127,6 +127,15 @@ class HttpGoogleDirectoryDAO(appName: String,
       ()
     }) {
       case e: HttpResponseException if e.getStatusCode == StatusCodes.NotFound.intValue => () //if the member is already absent, then don't keep trying to delete them
+    }.recoverWith {
+      case e: HttpResponseException if e.getStatusCode == StatusCodes.BadRequest.intValue => {
+        // removing a user that does not exist sometimes returns a 400 error, in that case check google to see
+        // if the user exists in the group and if it does not, ignore the error, if so, throw it
+        isGroupMember(groupEmail, memberEmail).flatMap {
+          case true => Future.failed(e)
+          case false => Future.unit
+        }
+      }
     }
   }
 
