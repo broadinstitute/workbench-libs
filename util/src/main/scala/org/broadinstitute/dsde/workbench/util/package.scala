994075e5c07c197441c148dc3226cf9d471ff202
Qi Wang <qi@broadinstitute.org>
2019-06-12 23:34:37 -0400

    cross build newrelic

--- util/src/main/scala/org/broadinstitute/dsde/workbench/util/package.scala
+++ util/src/main/scala/org/broadinstitute/dsde/workbench/util/package.scala
@@ -3,13 +3,12 @@ package org.broadinstitute.dsde.workbench
 import java.io.FileInputStream
 import java.nio.file.Path
 
-import cats.effect.{Concurrent, ContextShift, Resource, Sync}
+import cats.effect.{Blocker, Concurrent, ContextShift, Resource, Sync}
 import io.circe.Decoder
 import io.circe.fs2.decoder
 import fs2.{Stream, io}
 import org.typelevel.jawn.AsyncParser
 
-import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.global
 import scala.concurrent.duration._
 
@@ -47,8 +46,8 @@ package object util {
    * res0: List[String] = List(this is great)
    *
    */
-  def readJsonFileToA[F[_]: Sync: ContextShift: Concurrent, A: Decoder](path: Path, blockingExecutionContext: Option[ExecutionContext] = None): Stream[F, A] = {
-    io.file.readAll[F](path, blockingExecutionContext.getOrElse(global), 4096)
+  def readJsonFileToA[F[_]: Sync: ContextShift: Concurrent, A: Decoder](path: Path, blocker: Blocker = Blocker.liftExecutionContext(global)): Stream[F, A] = {
+    io.file.readAll[F](path, blocker, 4096)
       .through(fs2.text.utf8Decode)
       .through(_root_.io.circe.fs2.stringParser(AsyncParser.SingleValue))
       .through(decoder)
