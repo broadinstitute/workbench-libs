2e5af88932024c800f34967bf19a8df6ad7b555e
Kristian Cibulskis <kcibul@broadinstitute.org>
2019-08-05 13:34:14 -0400

    first pass

---
+++ model/src/main/scala/org/broadinstitute/dsde/workbench/model/WorkbenchProjectLocation.scala
@@ -0,0 +1,23 @@
+package org.broadinstitute.dsde.workbench.model
+
+sealed case class WorkbenchProjectLocation(name : String, storageClass : String, storageLocation: String, computeZones : Seq[String] )
+
+object WorkbenchProjectLocation {
+
+  // empty, default to US
+  object US extends WorkbenchProjectLocation("us", "multi_regional", "us", List("us-central1-b", "us-central1-c", "us-central1-f"))
+
+  object Finland extends WorkbenchProjectLocation("finland", "regional", "europe-north1", List("europe-north1-a", "europe-north1-a", "europe-north1-c"))
+  object Japan extends WorkbenchProjectLocation("japan", "regional", "asia-northeast1", List("asia-northeast1-a","asia-northeast1-b","asia-northeast1-c"))
+  object Australia extends WorkbenchProjectLocation( name = "australia", "regional","australia-southeast1", List("australia-southeast1-a", "australia-southeast1-b", "australia-southeast1-c"))
+
+  def fromName(name: String): Option[WorkbenchProjectLocation] = name.toLowerCase match {
+    case US.name => Some(US)
+    case "" => Some(US)
+    case Finland.name => Some(Finland)
+    case Japan.name => Some(Japan)
+    case Australia.name => Some(Australia)
+    case _ => None
+  }
+}
+
