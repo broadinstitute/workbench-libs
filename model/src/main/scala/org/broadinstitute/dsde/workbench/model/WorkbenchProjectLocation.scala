eb09cf4ab4f94f9f51765d2df5e71eacb6cb9a43
Kristian Cibulskis <kcibul@broadinstitute.org>
2019-08-05 14:00:50 -0400

    first pass

--- model/src/main/scala/org/broadinstitute/dsde/workbench/model/WorkbenchProjectLocation.scala
+++ model/src/main/scala/org/broadinstitute/dsde/workbench/model/WorkbenchProjectLocation.scala
@@ -1,15 +1,16 @@
 package org.broadinstitute.dsde.workbench.model
 
-sealed case class WorkbenchProjectLocation(name : String, storageClass : String, storageLocation: String, computeZones : Seq[String] )
+sealed case class WorkbenchProjectLocation(name : String, storageClass : String, storageLocation: String, computeRegionAndZones : Map[String, Seq[String]] )
 
 object WorkbenchProjectLocation {
 
   // empty, default to US
-  object US extends WorkbenchProjectLocation("us", "multi_regional", "us", List("us-central1-b", "us-central1-c", "us-central1-f"))
+  object US extends WorkbenchProjectLocation("us", "multi_regional", "us",
+   Map("us-central1" -> List("us-central1-b", "us-central1-c", "us-central1-f")) )
 
-  object Finland extends WorkbenchProjectLocation("finland", "regional", "europe-north1", List("europe-north1-a", "europe-north1-a", "europe-north1-c"))
-  object Japan extends WorkbenchProjectLocation("japan", "regional", "asia-northeast1", List("asia-northeast1-a","asia-northeast1-b","asia-northeast1-c"))
-  object Australia extends WorkbenchProjectLocation( name = "australia", "regional","australia-southeast1", List("australia-southeast1-a", "australia-southeast1-b", "australia-southeast1-c"))
+  object Finland extends WorkbenchProjectLocation("finland", "regional", "europe-north1", Map("europe-north1" -> List("europe-north1-a", "europe-north1-a", "europe-north1-c")))
+  object Japan extends WorkbenchProjectLocation("japan", "regional", "asia-northeast1", Map("asia-northeast1" -> List("asia-northeast1-a","asia-northeast1-b","asia-northeast1-c")))
+  object Australia extends WorkbenchProjectLocation( name = "australia", "regional","australia-southeast1", Map("australia-southeast1" -> List("australia-southeast1-a", "australia-southeast1-b", "australia-southeast1-c")))
 
   def fromName(name: String): Option[WorkbenchProjectLocation] = name.toLowerCase match {
     case US.name => Some(US)
