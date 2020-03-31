21b14ac9a9171e491dbb9a32710575bb1bf58d79
jdcanas <jcanas@broadinstitute.com>
2020-03-31 10:01:19 -0400

    bumping settings

--- project/Settings.scala
+++ project/Settings.scala
@@ -189,7 +189,7 @@ object Settings {
   val google2Settings = only212 ++ commonSettings ++ List(
     name := "workbench-google2",
     libraryDependencies ++= google2Dependencies,
-    version := createVersion("0.7")
+    version := createVersion("0.8")
   ) ++ publishSettings
 
   val newrelicSettings = only212 ++ commonSettings ++ List(
