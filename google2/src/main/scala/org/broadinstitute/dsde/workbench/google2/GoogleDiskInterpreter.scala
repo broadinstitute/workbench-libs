e6083a496cf2de8f39eb2d625f05414c4c0cc224
Gabriela <gcarrill@broadinstitute.org>
2020-05-13 16:22:58 -0400

    add new DiskService and begin DiskInterpreter

---
+++ google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleDiskInterpreter.scala
@@ -0,0 +1,35 @@
+package org.broadinstitute.dsde.workbench.google2
+
+import cats.effect.concurrent.Semaphore
+import cats.effect.{Async, Blocker, ContextShift, Timer}
+import cats.mtl.ApplicativeAsk
+import com.google.cloud.compute.v1.{Disk, DiskClient, Operation, ProjectZoneName}
+import io.chrisdavenport.log4cats.StructuredLogger
+import org.broadinstitute.dsde.workbench.RetryConfig
+import org.broadinstitute.dsde.workbench.model.TraceId
+import org.broadinstitute.dsde.workbench.model.google.GoogleProject
+
+private[google2] class GoogleDiskInterpreter[F[_]: Async: StructuredLogger: Timer: ContextShift] (
+  diskClient: DiskClient,
+  retryConfig: RetryConfig,
+  blocker: Blocker,
+  blockerBound: Semaphore[F]
+) extends GoogleDiskService[F] {
+
+  override def createDisk(project: GoogleProject, zone: ZoneName, disk: Disk)
+    (implicit ev: ApplicativeAsk[F, TraceId]
+  ): F[Operation] = {
+    val projectZone = ProjectZoneName.of(project.value, zone.value)
+    retryF(
+      Async[F].delay(diskClient.insertDisk(projectZone, disk)),
+      s"com.google.cloud.compute.v1DiskClient.insertDisk(${projectZone.toString}, ${disk.getName})"
+    )
+  }
+
+  private def getZoneName(zoneUrl: String): Option[ZoneName] =
+    Option(zoneUrl).flatMap(_.split("/").lastOption).map(ZoneName)
+
+  private def retryF[A](fa: F[A], loggingMsg: String)(implicit ev: ApplicativeAsk[F, TraceId]): F[A] =
+    tracedRetryGoogleF(retryConfig)(blockerBound.withPermit(blocker.blockOn(fa)), loggingMsg).compile.lastOrError
+
+}
