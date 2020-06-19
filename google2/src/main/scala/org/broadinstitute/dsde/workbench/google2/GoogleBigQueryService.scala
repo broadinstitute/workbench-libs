d7c79bceaec06b1522bba178fc1932583110631f
Qi Wang <qi@broadinstitute.org>
2020-06-19 08:41:55 -0400

    scalafmt

--- google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleBigQueryService.scala
+++ google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleBigQueryService.scala
@@ -6,7 +6,6 @@ import com.google.cloud.bigquery.BigQueryOptions.DefaultBigQueryFactory
 import com.google.cloud.bigquery.{BigQuery, BigQueryOptions, JobId, QueryJobConfiguration, TableResult}
 import io.chrisdavenport.log4cats.StructuredLogger
 
-
 trait GoogleBigQueryService[F[_]] {
   def query(queryJobConfiguration: QueryJobConfiguration, options: BigQuery.JobOption*): F[TableResult]
 
@@ -14,22 +13,26 @@ trait GoogleBigQueryService[F[_]] {
 }
 
 object GoogleBigQueryService {
-  def resource[F[_]: Sync: ContextShift: Timer: StructuredLogger]
-  (pathToJson: String, blocker: Blocker): Resource[F, GoogleBigQueryService[F]] =
+  def resource[F[_]: Sync: ContextShift: Timer: StructuredLogger](
+    pathToJson: String,
+    blocker: Blocker
+  ): Resource[F, GoogleBigQueryService[F]] =
     credentialResource(pathToJson) flatMap (resource(_, blocker))
 
-  def resource[F[_]: Sync: ContextShift: Timer: StructuredLogger]
-  (credentials: Credentials, blocker: Blocker): Resource[F, GoogleBigQueryService[F]] =
+  def resource[F[_]: Sync: ContextShift: Timer: StructuredLogger](
+    credentials: Credentials,
+    blocker: Blocker
+  ): Resource[F, GoogleBigQueryService[F]] =
     for {
       client <- Resource.liftF[F, BigQuery](
         Sync[F].delay(
           new DefaultBigQueryFactory().create(
-            BigQueryOptions.newBuilder()
+            BigQueryOptions
+              .newBuilder()
               .setCredentials(credentials)
-              .build())
+              .build()
+          )
         )
       )
     } yield new GoogleBigQueryInterpreter[F](client, blocker)
 }
-
-
