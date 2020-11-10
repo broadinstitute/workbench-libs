c355003c99d9826d98f4aab7b31d8e331999c531
Justin Canas <justincanas16@gmail.com>
2020-11-10 13:26:04 -0500

    [IA-2305] Add publshOne to GooglePublisher and add a mock for it  (#369)
    * adding publishOne to GooglePublosher and FakeGooglePublisher mock
    
    * fmt
    
    * travis pls
    
    * changelog fix
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

---
+++ google2/src/test/scala/org/broadinstitute/dsde/workbench/google2/mock/FakeGooglePublisher.scala
@@ -0,0 +1,21 @@
+package org.broadinstitute.dsde.workbench.google2.mock
+
+import cats.effect.{Concurrent, IO}
+import cats.mtl.Ask
+import com.google.pubsub.v1.PubsubMessage
+import fs2.Pipe
+import io.circe.Encoder
+import org.broadinstitute.dsde.workbench.google2.GooglePublisher
+import org.broadinstitute.dsde.workbench.google2.JsonCodec._
+import org.broadinstitute.dsde.workbench.model.TraceId
+
+class FakeGooglePublisher extends GooglePublisher[IO] {
+  override def publish[MessageType: Encoder]: Pipe[IO, MessageType, Unit] =
+    in => in.evalMap(_ => IO.unit)
+
+  override def publishNative: Pipe[IO, PubsubMessage, Unit] = in => in.evalMap(_ => IO.unit)
+
+  override def publishString: Pipe[IO, String, Unit] = in => in.evalMap(_ => IO.unit)
+
+  override def publishOne[MessageType: Encoder](message: MessageType)(implicit ev: Ask[IO, TraceId]): IO[Unit] = IO.unit
+}
