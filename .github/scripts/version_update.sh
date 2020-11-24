fc70dec7f27ae0d583f0879904ed03b05c440b65
Qi Wang <qi@broadinstitute.org>
2020-11-24 15:49:18 -0500

    try fix version update

--- .github/scripts/version_update.sh
+++ .github/scripts/version_update.sh
@@ -15,36 +15,21 @@ TO_REPLACE="TRAVIS-REPLACE-ME"
 
 HASH=$(git rev-parse HEAD | cut -c -7)
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+git config --global user.email "broadbot@broadinstitute.org"
+git config --global user.name "broadbot"
 
-# Don't run twice: prevent Scala 2.11 runs
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
-  echo "Auto-updating version hashes..."
+echo "Auto-updating version hashes..."
 
-  # ignore failure if the remote does not exist
-  git remote rm ${REMOTE_NAME} || true
-  git remote add ${REMOTE_NAME} https://${GH_TOKEN}@github.com/broadinstitute/workbench-libs.git > /dev/null 2>&1
-  git fetch ${REMOTE_NAME}
-  git checkout ${REMOTE_NAME}/${PUSH_BRANCH}
+for f in README.md */CHANGELOG.md
+  do
+    echo "Replacing ${TO_REPLACE} with ${HASH} in ${f}..."
+    sed -i "s/${TO_REPLACE}/${HASH}/g" ${f}
 
-  for f in README.md */CHANGELOG.md
-    do
-      echo "Replacing ${TO_REPLACE} with ${HASH} in ${f}..."
-      sed -i "s/${TO_REPLACE}/${HASH}/g" ${f}
+    # will do nothing if there's nothing to add
+    git add ${f}
 
-      # will do nothing if there's nothing to add
-      git add ${f}
+    # will fail if no change: ignore it
+    git commit --message "Auto update hash in ${f} to ${HASH}" || true
+  done
 
-      # will fail if no change: ignore it
-      git commit --message "Auto update hash in ${f} to ${HASH}" || true
-    done
-
-  # push one commit per update.  Will fail if no changes: ignore it
-  git push ${REMOTE_NAME} HEAD:${PUSH_BRANCH} || true
-
-  git remote rm ${REMOTE_NAME}
-  git checkout -
-
-  echo "Finished updating version hashes."
-fi
+echo "Finished updating version hashes."
