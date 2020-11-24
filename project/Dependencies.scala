6de5d652f5fdce2bd815c5be4c3e5832c911f2ef
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-24 03:11:51 +0100

    Update google-cloud-pubsub to 1.105.1 (#388)
    * Update google-cloud-pubsub to 1.105.1
    
    * Update google-cloud-pubsub to 1.105.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
   val googleFirestore: ModuleID = "com.google.cloud" % "google-cloud-firestore" % "1.33.0"
   val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "1.113.4"
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.122.1" % "test"
-  val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.105.0"
+  val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.105.1"
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "0.77.0-beta"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "0.122.1"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "0.118.0-alpha"
