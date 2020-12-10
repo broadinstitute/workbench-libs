a7a5b9f711ace6c5f18d51c3265dd06e38d85f21
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-10 19:36:13 +0100

    Update google-cloud-bigquery from 1.125.0 to 1.125.2 (#427)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,7 +63,7 @@ object Dependencies {
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "0.118.0-alpha"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "1.2.1"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "10.0.0"
-  val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "1.125.0"
+  val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "1.125.2"
   //the below v1 module is a dependency for v2 because it contains the OAuth scopes necessary to created scoped credentials
   val googleContainerV1: ModuleID = "com.google.apis" % "google-api-services-container" % "v1-rev20201007-1.31.0"
 
