001ba9b129fb1aa4c0597a193d671a81c058a7d1
Greg Polumbo <gpolumbo@broadinstitute.org>
2021-01-14 15:39:29 -0500

    SUP-437 Commenting out the the `-Xcheckinit` compiler flag

--- project/Settings.scala
+++ project/Settings.scala
@@ -38,7 +38,7 @@ object Settings {
         "-language:higherKinds", // Allow higher-kinded types
         "-language:implicitConversions", // Allow definition of implicit functions called views
         "-unchecked", // Enable additional warnings where generated code depends on assumptions.
-        "-Xcheckinit", // Wrap field accessors to throw an exception on uninitialized access.
+//        "-Xcheckinit", // Wrap field accessors to throw an exception on uninitialized access.
 //        "-Xfatal-warnings", // Fail the compilation if there are any warnings.
         "-Xfuture", // Turn on future language features.
         "-Xlint:adapted-args", // Warn if an argument list is modified to match the receiver.
@@ -81,7 +81,7 @@ object Settings {
         "-language:higherKinds", // Allow higher-kinded types
         "-language:implicitConversions", // Allow definition of implicit functions called views
         "-unchecked", // Enable additional warnings where generated code depends on assumptions.
-        "-Xcheckinit", // Wrap field accessors to throw an exception on uninitialized access.
+//        "-Xcheckinit", // Wrap field accessors to throw an exception on uninitialized access.
 //      "-Xfatal-warnings", // Fail the compilation if there are any warnings.
         "-Xlint:adapted-args", // Warn if an argument list is modified to match the receiver.
         "-Xlint:constant", // Evaluation of a constant arithmetic expression results in an error.
