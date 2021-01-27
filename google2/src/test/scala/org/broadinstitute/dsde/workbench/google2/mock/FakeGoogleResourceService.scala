40ca1d59e8cef6a8905095f4466fc786790e6fc3
jdcanas <jcanas@broadinstitute.com>
2021-01-27 14:00:58 -0500

    add mock

---
+++ google2/src/test/scala/org/broadinstitute/dsde/workbench/google2/mock/FakeGoogleResourceService.scala
@@ -0,0 +1,16 @@
+package org.broadinstitute.dsde.workbench.google2.mock
+
+import cats.effect.IO
+import cats.mtl.Ask
+import com.google.cloud.resourcemanager.Project
+import org.broadinstitute.dsde.workbench.google2.GoogleResourceService
+import org.broadinstitute.dsde.workbench.model.TraceId
+import org.broadinstitute.dsde.workbench.model.google.GoogleProject
+
+class FakeGoogleResourceService extends GoogleResourceService[IO] {
+  override def getProject(project: GoogleProject)(implicit ev: Ask[IO, TraceId]): IO[Option[Project]] = IO(None)
+
+  override def listProject(filter: Option[GoogleProject])(implicit ev: Ask[IO, TraceId]): IO[List[Project]] = IO(List.empty)
+}
+
+object FakeGoogleResourceService extends FakeGoogleResourceService
