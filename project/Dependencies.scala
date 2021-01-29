05a076572ab5103df372977bc977f36fdc6e6c54
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-29 16:17:56 +0100

    Update google-cloud-firestore from 2.1.0 to 2.2.0 (#481)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
   val googleRpc: ModuleID =               "io.grpc" % "grpc-core" % "1.34.0"
 
   val googleRpc2: ModuleID =               "io.grpc" % "grpc-core" % "1.34.0"
-  val googleFirestore: ModuleID = "com.google.cloud" % "google-cloud-firestore" % "2.1.0"
+  val googleFirestore: ModuleID = "com.google.cloud" % "google-cloud-firestore" % "2.2.0"
   val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "1.113.8"
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.122.3" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.110.3"
