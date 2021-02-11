10ef29b577305ab53004e89bbbcccb629f3e3992
ansingh7115 <singh.an@husky.neu.edu>
2021-02-10 19:25:57 -0500

    fix formatting, try again

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/dao/Google.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 import akka.actor.ActorSystem
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.storage.StorageOptions
+import com.google.cloud.storage.{Storage, StorageOptions}
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.ServiceTestConfig
 import org.broadinstitute.dsde.workbench.google._
@@ -36,7 +36,7 @@ object Google {
   lazy val googleDirectoryDAO =
     new HttpGoogleDirectoryDAO(appName, pemModeWithServiceAccountUser, metricBaseName)(system, ec)
 
-  def storageOptions(projectName: String, googleCredentials: GoogleCredentials) = StorageOptions.newBuilder
+  def storageOptions(projectName: String, googleCredentials: GoogleCredentials): Storage = StorageOptions.newBuilder
     .setProjectId(projectName)
     .setCredentials(googleCredentials)
     .build.getService
