0cb810b8a6ab3f26550b8c6688553708da5af6d3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-17 21:33:49 +0100

    Update http4s-blaze-client, http4s-circe, ... from 0.21.16 to 0.21.18 (#499)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scalaLoggingV = "3.9.2"
   val scalaTestV    = "3.2.3"
   val circeVersion = "0.13.0"
-  val http4sVersion = "0.21.16"
+  val http4sVersion = "0.21.18"
 
   def excludeGuavaJDK5(m: ModuleID): ModuleID = m.exclude("com.google.guava", "guava-jdk5")
 
