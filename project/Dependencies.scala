021cbd6812a084bbc7a061cd989d6f2f352d7dd6
Scala Steward <me@scala-steward.org>
2021-02-23 20:17:06 +0100

    Update google-cloud-dataproc from 1.1.10 to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies {
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.122.5" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.111.0"
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "1.40.5"
-  val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "1.1.10"
+  val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "1.2.0"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "0.118.0-alpha"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "1.2.4"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "11.0.0"
