634d4a8ee259dc0978abd0aa318d96cec50b1ded
Scala Steward <me@scala-steward.org>
2021-02-23 20:16:25 +0100

    Update google-api-services-container from v1-rev20201209-1.31.0 to v1-rev20210216-1.31.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "1.1.12"
   val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "0.118.8-alpha"
   //the below v1 module is a dependency for v2 because it contains the OAuth scopes necessary to created scoped credentials
-  val googleContainerV1: ModuleID = "com.google.apis" % "google-api-services-container" % "v1-rev20201209-1.31.0"
+  val googleContainerV1: ModuleID = "com.google.apis" % "google-api-services-container" % "v1-rev20210216-1.31.0"
 
 
   val circeCore: ModuleID = "io.circe" %% "circe-core" % circeVersion
