3ee575d01384b08092c565aef1ac9410555a4f66
Scala Steward <me@scala-steward.org>
2021-03-31 18:15:55 +0200

    Update google-cloud-resourcemanager from 0.118.8-alpha to 0.118.12-alpha

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -66,7 +66,7 @@ object Dependencies {
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "11.0.0"
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "1.127.7"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "1.1.15"
-  val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "0.118.8-alpha"
+  val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "0.118.12-alpha"
   //the below v1 module is a dependency for v2 because it contains the OAuth scopes necessary to created scoped credentials
   val googleContainerV1: ModuleID = "com.google.apis" % "google-api-services-container" % "v1-rev20201209-1.31.0"
 
