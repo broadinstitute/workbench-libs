cbcf2bbdcd038e3e086ea0e284cd55d2a8293052
Scala Steward <me@scala-steward.org>
2021-04-10 02:10:45 +0200

    Update client-java from 11.0.1 to 12.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -62,7 +62,7 @@ object Dependencies {
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "1.3.0"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "0.118.0-alpha"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "1.3.0"
-  val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "11.0.1"
+  val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "12.0.0"
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "1.127.11"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "1.1.15"
   val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "0.118.12-alpha"
