274cc15954ec22c97ff221200c4d501ff98d4df6
Scala Steward <me@scala-steward.org>
2021-06-03 16:09:55 +0200

    Update google-cloud-container from 1.3.3 to 1.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "1.40.8"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "1.4.2"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "0.119.11-alpha"
-  val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "1.3.3"
+  val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "1.3.4"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "11.0.1"
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "1.127.12"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "1.1.18"
