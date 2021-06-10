2ef52b335a3da996018a02cc0f822db4b561be88
Scala Steward <me@scala-steward.org>
2021-06-10 14:10:15 +0200

    Update scala-library from 2.12.12 to 2.12.14

--- project/Settings.scala
+++ project/Settings.scala
@@ -112,7 +112,7 @@ object Settings {
   })
 
   val cross212and213 = Seq(
-    crossScalaVersions := List("2.12.12", "2.13.5")
+    crossScalaVersions := List("2.12.14", "2.13.5")
   )
 
   //common settings for all sbt subprojects
