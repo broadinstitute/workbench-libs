71c1f2080a3ea66d0088f4d8ac78b912e3f97d33
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-09 16:27:02 +0200

    Update akka-actor, akka-stream, ... from 2.6.15 to 2.6.16 (#725)
    * Update akka-actor, akka-stream, ... from 2.6.15 to 2.6.16
    
    * Update akka-actor, akka-stream, ... from 2.6.15 to 2.6.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val akkaV         = "2.6.15"
+  val akkaV         = "2.6.16"
   val akkaHttpV     = "10.2.6"
   val jacksonV      = "2.12.4"
   val googleV       = "1.22.0"
