8b8214a4d41fae1fabcc8caa1aa423a48a4eb698
Edmund Higham <edhigham@gmail.com>
2021-10-19 16:11:15 -0400

    implement `transferBucket`. TODO: add test

--- google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferInterpreter.scala
+++ google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferInterpreter.scala
@@ -1,15 +1,13 @@
-package org.broadinstitute.dsde.workbench
-package google2
+package org.broadinstitute.dsde.workbench.google2
 
 import java.nio.file.Paths
-
 import cats.effect._
 import cats.effect.std.Semaphore
 import com.google.auth.oauth2.ServiceAccountCredentials
 import com.google.`type`.Date
 import com.google.cloud.storage.{Storage, StorageOptions}
 import com.google.storagetransfer.v1.proto.StorageTransferServiceClient
-import com.google.storagetransfer.v1.proto.TransferTypes.GcsData
+import com.google.storagetransfer.v1.proto.TransferTypes.{GcsData, TransferJob, TransferSpec}
 import com.google.storagetransfer.v1.proto.{TransferProto, TransferTypes}
 import io.kubernetes.client.proto.Meta.Timestamp
 import fs2.Stream
@@ -19,21 +17,17 @@ import io.circe.fs2._
 import org.broadinstitute.dsde.workbench.model.google.GoogleProject
 import org.typelevel.log4cats.StructuredLogger
 
-private[google2] class GoogleStorageTransferInterpreter[F[_]](
-                                                       db: Storage,
-                                                       blockerBound: Option[Semaphore[F]]
-                                                     )(implicit logger: StructuredLogger[F], F: Async[F])
+import scala.util.Using
+
+class GoogleStorageTransferInterpreter[F[_]]()(implicit logger: StructuredLogger[F], F: Async[F])
   extends GoogleStorageTransferService[F] {
 
-
-
-
-  def makeTransferJobSchedule(schedule: StorageTransferJobSchedule): TransferTypes.Schedule = schedule match {
-      case Once(time) => TransferTypes.Schedule.newBuilder()
-        .setScheduleStartDate(time)
-        .setScheduleEndDate(time)
-        .build
-    }
+  private def makeTransferJobSchedule(schedule: StorageTransferJobSchedule): TransferTypes.Schedule = schedule match {
+    case Once(time) => TransferTypes.Schedule.newBuilder()
+      .setScheduleStartDate(time)
+      .setScheduleEndDate(time)
+      .build
+  }
 
   override def transferBucket(jobName: String,
                               jobDescription: String,
@@ -41,67 +35,22 @@ private[google2] class GoogleStorageTransferInterpreter[F[_]](
                               originBucket: String,
                               destinationBucket: String,
                               schedule: StorageTransferJobSchedule
-                             ): F[Unit] = {
-  val client = StorageTransferServiceClient.create()
-  val transferJob = TransferTypes.TransferJob.newBuilder
-    .setName(jobName)
-    .setDescription(jobDescription)
-    .setProjectId(projectToBill.value)
-    .setTransferSpec(TransferTypes.TransferSpec.newBuilder
-      .setGcsDataSource(GcsData.newBuilder().setBucketName(originBucket).build)
-      .setGcsDataSink(GcsData.newBuilder().setBucketName(destinationBucket).build)
-      .build)
-    .setSchedule(makeTransferJobSchedule(schedule))
-    .setCreationTime(Timestamp.newBuilder.build)
-    .setLastModificationTime(Timestamp.newBuilder.build)
-    .setDeletionTime(Timestamp.newBuilder.build)
-    .setLatestOperationName("latestOperationName-1244328885")
-    .build
+                             ): F[TransferJob] = {
+    val transferJob = TransferJob.newBuilder
+      .setName(jobName)
+      .setDescription(jobDescription)
+      .setProjectId(projectToBill.value)
+      .setTransferSpec(TransferSpec.newBuilder
+        .setGcsDataSource(GcsData.newBuilder().setBucketName(originBucket).build)
+        .setGcsDataSink(GcsData.newBuilder().setBucketName(destinationBucket).build)
+        .build)
+      .setSchedule(makeTransferJobSchedule(schedule))
+      .build
 
     val request = TransferProto.CreateTransferJobRequest.newBuilder
       .setTransferJob(transferJob)
       .build
-    client.createTransferJob(request)
+
+    Async[F].delay(Using.resource(StorageTransferServiceClient.create)(_.createTransferJob(request)))
   }
-}
-
-object GoogleStorageInterpreter {
-  def apply[F[_]: Async: StructuredLogger](
-                                            db: Storage,
-                                            blockerBound: Option[Semaphore[F]]
-                                          ): GoogleStorageInterpreter[F] =
-    new GoogleStorageInterpreter(db, blockerBound)
-
-  def storage[F[_]: Sync: Files](
-                                  pathToJson: String,
-                                  project: Option[GoogleProject] =
-                                  None // legacy credential file doesn't have `project_id` field. Hence we need to pass in explicitly
-                                ): Resource[F, Storage] =
-    for {
-      credential <- org.broadinstitute.dsde.workbench.util2.readFile(pathToJson)
-      project <- project match { //Use explicitly passed in project if it's defined; else use `project_id` in json credential; if neither has project defined, raise error
-        case Some(p) => Resource.pure[F, GoogleProject](p)
-        case None    => Resource.eval(parseProject(pathToJson).compile.lastOrError)
-      }
-      db <- Resource.eval(
-        Sync[F].delay(
-          StorageOptions
-            .newBuilder()
-            .setCredentials(ServiceAccountCredentials.fromStream(credential))
-            .setProjectId(project.value)
-            .build()
-            .getService
-        )
-      )
-    } yield db
-
-  implicit val googleProjectDecoder: Decoder[GoogleProject] = Decoder.forProduct1(
-    "project_id"
-  )(GoogleProject.apply)
-
-  def parseProject[F[_]: Files: Sync](pathToJson: String): Stream[F, GoogleProject] =
-    Files[F]
-      .readAll(Paths.get(pathToJson), 4096)
-      .through(byteStreamParser)
-      .through(decoder[F, GoogleProject])
 }
--- google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferService.scala
+++ google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferService.scala
@@ -2,7 +2,6 @@ package org.broadinstitute.dsde.workbench
 package google2
 
 import java.nio.file.Path
-
 import cats.data.NonEmptyList
 import cats.effect._
 import cats.effect.std.Semaphore
@@ -15,6 +14,7 @@ import com.google.cloud.storage.{Acl, Blob, BlobId, Bucket, StorageOptions}
 import com.google.cloud.{Identity, Policy}
 import fs2.{Pipe, Stream}
 import com.google.cloud.storage.Storage.{BucketGetOption, BucketSourceOption}
+import com.google.storagetransfer.v1.proto.TransferTypes.TransferJob
 import org.typelevel.log4cats.StructuredLogger
 import org.broadinstitute.dsde.workbench.google2.util.RetryPredicates.standardGoogleRetryConfig
 import org.broadinstitute.dsde.workbench.model.TraceId
@@ -29,92 +29,13 @@ import scala.language.higherKinds
  */
 trait GoogleStorageTransferService[F[_]] {
   sealed trait StorageTransferJobSchedule
-
   case class Once(time: Date) extends StorageTransferJobSchedule
 
-  def transferBucket(): F[Unit]
-
-}
-
-object GoogleStorageService {
-  def resource[F[_]: Async: StructuredLogger](
-                                               pathToCredentialJson: String,
-                                               blockerBound: Option[Semaphore[F]] = None,
-                                               project: Option[GoogleProject] = None
-                                             ): Resource[F, GoogleStorageService[F]] =
-    for {
-      db <- GoogleStorageInterpreter.storage[F](pathToCredentialJson, project)
-    } yield GoogleStorageInterpreter[F](db, blockerBound)
-
-  def fromApplicationDefault[F[_]: Async: StructuredLogger](
-                                                             blockerBound: Option[Semaphore[F]] = None
-                                                           ): Resource[F, GoogleStorageService[F]] =
-    for {
-      db <- Resource.eval(
-        Sync[F].delay(
-          StorageOptions
-            .newBuilder()
-            .setCredentials(GoogleCredentials.getApplicationDefault())
-            .build()
-            .getService
-        )
-      )
-    } yield GoogleStorageInterpreter[F](db, blockerBound)
-
-  def fromAccessToken[F[_]: Async: StructuredLogger](
-                                                      accessToken: AccessToken,
-                                                      blockerBound: Option[Semaphore[F]] = None
-                                                    ): Resource[F, GoogleStorageService[F]] =
-    for {
-      db <- Resource.eval(
-        Sync[F].delay(
-          StorageOptions
-            .newBuilder()
-            .setCredentials(GoogleCredentials.create(accessToken))
-            .build()
-            .getService
-        )
-      )
-    } yield GoogleStorageInterpreter[F](db, blockerBound)
-}
-
-final case class GcsBlobName(value: String) extends AnyVal
-
-sealed trait RemoveObjectResult extends Product with Serializable
-object RemoveObjectResult {
-  def apply(res: Boolean): RemoveObjectResult = if (res) Removed else NotFound
-
-  final case object Removed extends RemoveObjectResult
-  final case object NotFound extends RemoveObjectResult
-}
-
-sealed abstract class StorageRole extends Product with Serializable {
-  def name: String
-}
-object StorageRole {
-  final case object ObjectCreator extends StorageRole {
-    def name: String = "roles/storage.objectCreator"
-  }
-  final case object ObjectViewer extends StorageRole {
-    def name: String = "roles/storage.objectViewer"
-  }
-  final case object ObjectAdmin extends StorageRole {
-    def name: String = "roles/storage.objectAdmin"
-  }
-  final case object StorageAdmin extends StorageRole {
-    def name: String = "roles/storage.admin"
-  }
-  //The custom roleId must be in the form "organizations/{organization_id}/roles/{role}",
-  //or "projects/{project_id}/roles/{role}"
-  final case class CustomStorageRole(roleId: String) extends StorageRole {
-    def name: String = roleId
-  }
-}
-
-final case class Crc32(asString: String) extends AnyVal
-sealed abstract class GetMetadataResponse extends Product with Serializable
-object GetMetadataResponse {
-  final case object NotFound extends GetMetadataResponse
-  final case class Metadata(crc32: Crc32, userDefined: Map[String, String], generation: Long)
-    extends GetMetadataResponse
+  def transferBucket(jobName: String,
+                     jobDescription: String,
+                     projectToBill: GoogleProject,
+                     originBucket: String,
+                     destinationBucket: String,
+                     schedule: StorageTransferJobSchedule
+                    ): F[TransferJob]
 }
