a777759236c89b857bcfae8f56947ea6c9785acb
Edmund Higham <edhigham@gmail.com>
2021-11-08 15:57:16 -0500

    pr suggestions from @Qi77Qi

--- google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferInterpreter.scala
+++ google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferInterpreter.scala
@@ -1,6 +1,7 @@
 package org.broadinstitute.dsde.workbench.google2
 
 import cats.effect._
+import cats.implicits.toFunctorOps
 import com.google.`type`.Date
 import com.google.longrunning.{ListOperationsRequest, Operation}
 import com.google.storagetransfer.v1.proto.TransferProto._
@@ -9,46 +10,30 @@ import com.google.storagetransfer.v1.proto.{StorageTransferServiceClient, Transf
 import org.broadinstitute.dsde.workbench.google2.GoogleStorageTransferService._
 import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
 import org.broadinstitute.dsde.workbench.model.google._
+import org.typelevel.log4cats.StructuredLogger
 
 import java.time.ZonedDateTime
 import scala.collection.JavaConverters._
 
 final private[google2] class GoogleStorageTransferInterpreter[F[_]](client: StorageTransferServiceClient)(implicit
-  F: Sync[F]
+  F: Sync[F] with Temporal[F],
+  logger: StructuredLogger[F]
 ) extends GoogleStorageTransferService[F] {
-
-  private object Date {
-    def fromZonedDateTime(datetime: ZonedDateTime): Date =
-      com.google.`type`.Date.newBuilder
-        .setYear(datetime.getYear)
-        .setMonth(datetime.getMonthValue)
-        .setDay(datetime.getDayOfMonth)
-        .build
-  }
-
-  private def makeSchedule(schedule: JobTransferSchedule): Schedule = schedule match {
-    case JobTransferSchedule.Immediately =>
-      val today = Date.fromZonedDateTime(ZonedDateTime.now)
-      Schedule.newBuilder
-        .setScheduleStartDate(today)
-        .setScheduleEndDate(today)
-        .clearStartTimeOfDay()
-        .build
-  }
 
   override def getStsServiceAccount(project: GoogleProject): F[ServiceAccount] = {
     val request = GetGoogleServiceAccountRequest.newBuilder
       .setProjectId(project.value)
       .build
 
-    F.delay {
-      val sa = client.getGoogleServiceAccount(request)
-      ServiceAccount(
-        ServiceAccountSubjectId(sa.getSubjectId),
-        WorkbenchEmail(sa.getAccountEmail),
-        ServiceAccountDisplayName(sa.getAccountEmail)
-      )
-    }
+    val getGoogleServiceAccount = F.delay(client.getGoogleServiceAccount(request))
+    withLogging(getGoogleServiceAccount, None, s"${client.getClass.getName}.getGoogleServiceAccount")
+      .map { sa =>
+        ServiceAccount(
+          ServiceAccountSubjectId(sa.getSubjectId),
+          WorkbenchEmail(sa.getAccountEmail),
+          ServiceAccountDisplayName(sa.getAccountEmail)
+        )
+      }
   }
 
   override def createTransferJob(jobName: JobName,
@@ -76,9 +61,8 @@ final private[google2] class GoogleStorageTransferInterpreter[F[_]](client: Stor
       .setTransferJob(transferJob)
       .build
 
-    F.delay {
-      client.createTransferJob(request)
-    }
+    val createTransferJob = F.delay(client.createTransferJob(request))
+    withLogging(createTransferJob, None, s"${client.getClass.getName}.createTransferJob")
   }
 
   override def getTransferJob(jobName: JobName, project: GoogleProject): F[TransferJob] = {
@@ -87,9 +71,8 @@ final private[google2] class GoogleStorageTransferInterpreter[F[_]](client: Stor
       .setProjectId(project.value)
       .build
 
-    F.delay {
-      client.getTransferJob(request)
-    }
+    val getTransferJob = F.delay(client.getTransferJob(request))
+    withLogging(getTransferJob, None, s"${client.getClass.getName}.getTransferJob")
   }
 
   override def listTransferOperations(jobName: JobName, project: GoogleProject): F[Seq[Operation]] = {
@@ -97,14 +80,34 @@ final private[google2] class GoogleStorageTransferInterpreter[F[_]](client: Stor
       .setFilter(s"""{"projectId":"$project","jobNames":["$jobName"]}""")
       .build
 
-    F.delay {
-      client.getOperationsClient.listOperations(request).iterateAll.asScala.toSeq
-    }
+    val operationsClient = client.getOperationsClient
+    val listOperations = F.delay(client.getOperationsClient.listOperations(request))
+    withLogging(listOperations, None, s"${operationsClient.getClass.getName}.listOperations")
+      .map(_.iterateAll.asScala.toSeq)
   }
 
-  override def getTransferOperation(operationName: OperationName): F[Operation] =
-    F.delay {
-      client.getOperationsClient.getOperation(operationName.value)
-    }
+  override def getTransferOperation(operationName: OperationName): F[Operation] = {
+    val operationsClient = client.getOperationsClient
+    val getOperation = F.delay(client.getOperationsClient.getOperation(operationName.value))
+    withLogging(getOperation, None, s"${operationsClient.getClass.getName}.getOperation")
+  }
 
+  private object Date {
+    def fromZonedDateTime(datetime: ZonedDateTime): Date =
+      com.google.`type`.Date.newBuilder
+        .setYear(datetime.getYear)
+        .setMonth(datetime.getMonthValue)
+        .setDay(datetime.getDayOfMonth)
+        .build
+  }
+
+  private def makeSchedule(schedule: JobTransferSchedule): Schedule = schedule match {
+    case JobTransferSchedule.Immediately =>
+      val today = Date.fromZonedDateTime(ZonedDateTime.now)
+      Schedule.newBuilder
+        .setScheduleStartDate(today)
+        .setScheduleEndDate(today)
+        .clearStartTimeOfDay()
+        .build
+  }
 }
--- google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferService.scala
+++ google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/GoogleStorageTransferService.scala
@@ -1,13 +1,16 @@
 package org.broadinstitute.dsde.workbench
 package google2
 
-import cats.effect.{Resource, Sync}
+import cats.effect.{Resource, Sync, Temporal}
+import com.google.api.gax.core.FixedCredentialsProvider
+import com.google.auth.oauth2.ServiceAccountCredentials
 import com.google.longrunning.Operation
-import com.google.storagetransfer.v1.proto.StorageTransferServiceClient
 import com.google.storagetransfer.v1.proto.TransferTypes.TransferJob
+import com.google.storagetransfer.v1.proto.{StorageTransferServiceClient, StorageTransferServiceSettings}
 import org.broadinstitute.dsde.workbench.google2.GoogleStorageTransferService._
 import org.broadinstitute.dsde.workbench.model.ValueObject
 import org.broadinstitute.dsde.workbench.model.google.{GcsBucketName, GoogleProject, ServiceAccount}
+import org.typelevel.log4cats.StructuredLogger
 
 trait GoogleStorageTransferService[F[_]] {
 
@@ -30,12 +33,12 @@ trait GoogleStorageTransferService[F[_]] {
 
 object GoogleStorageTransferService {
 
-  sealed trait JobTransferSchedule extends Any
+  sealed trait JobTransferSchedule extends Product with Serializable
 
   final object JobTransferSchedule {
 
     /** The job should run when the job is created. */
-    final object Immediately extends JobTransferSchedule
+    final case object Immediately extends JobTransferSchedule
   }
 
   final case class JobName(value: String) extends ValueObject
@@ -58,8 +61,24 @@ object GoogleStorageTransferService {
       else Left(s"""Illegal operation name - "$name" must start with "$prefix".""")
   }
 
-  def resource[F[_]](implicit F: Sync[F]): Resource[F, GoogleStorageTransferService[F]] =
+  def resource[F[_]](implicit
+    F: Sync[F] with Temporal[F],
+    logger: StructuredLogger[F]
+  ): Resource[F, GoogleStorageTransferService[F]] =
     Resource
       .fromAutoCloseable(F.delay(StorageTransferServiceClient.create))
       .map(new GoogleStorageTransferInterpreter[F](_))
+
+  def resource[F[_]](credential: ServiceAccountCredentials)(implicit
+    F: Sync[F] with Temporal[F],
+    logger: StructuredLogger[F]
+  ): Resource[F, GoogleStorageTransferService[F]] = {
+    val settings = StorageTransferServiceSettings.newBuilder
+      .setCredentialsProvider(FixedCredentialsProvider.create(credential))
+      .build
+
+    Resource
+      .fromAutoCloseable(F.delay(StorageTransferServiceClient.create(settings)))
+      .map(new GoogleStorageTransferInterpreter[F](_))
+  }
 }
