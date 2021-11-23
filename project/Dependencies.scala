1014ec48d9598c45ec1f6a96c1a6f009fecfafe6
Scala Steward <me@scala-steward.org>
2021-11-23 08:15:42 +0100

    Update scalacache-caffeine from 1.0.0-M5 to 1.0.0-M6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -95,7 +95,7 @@ object Dependencies {
   val openCensusTraceStackDriver: ModuleID = "io.opencensus" % "opencensus-exporter-trace-stackdriver" % "0.28.3"
   val openCensusTraceLogging: ModuleID = "io.opencensus" % "opencensus-exporter-trace-logging" % "0.26.0"
   val sealerate: ModuleID = "ca.mrvisser" %% "sealerate" % "0.0.6"
-  val scalaCache = "com.github.cb372" %% "scalacache-caffeine" % "1.0.0-M5"
+  val scalaCache = "com.github.cb372" %% "scalacache-caffeine" % "1.0.0-M6"
 
   val commonDependencies = Seq(
     scalatest,
