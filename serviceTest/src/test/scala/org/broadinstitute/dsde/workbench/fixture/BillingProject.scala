e1e13d938583cb1a92ee5bdb94bfa24a170e565a
Edmund Higham <edhigham@gmail.com>
2022-02-10 14:42:26 -0500

    expose a billing project `Resource`

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/BillingProject.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/fixture/BillingProject.scala
@@ -11,7 +11,7 @@ import org.broadinstitute.dsde.workbench.service.Orchestration
 
 import java.util.UUID
 
-object BillingFixtures {
+object BillingProject {
 
   /**
    * Create a new v2 billing project for the activation of `testCode`. The billing project will be
@@ -22,13 +22,13 @@ object BillingFixtures {
    * @param ownerEmails        List of billing project owner email addresses. [default: empty]
    * @param userEmails         List of billing project user email addresses [default: empty]
    * @param testCode           Code to exercise with new billing project
-   * @param ownerAuthToken     Auth token of billing project creator
+   * @param creatorAuthToken   Auth token of billing project creator
    */
   def withCleanBillingProject[A](billingAccountName: String,
                                  projectNamePrefix: String = "tmp-billing-project-",
                                  ownerEmails: List[String] = List.empty,
                                  userEmails: List[String] = List.empty
-  )(testCode: String => A)(implicit ownerAuthToken: AuthToken): A =
+  )(testCode: String => A)(implicit creatorAuthToken: AuthToken): A =
     withCleanBillingProjectF(billingAccountName, projectNamePrefix, ownerEmails, userEmails) { billingProject =>
       IO(testCode(billingProject))
     }.unsafeRunSync
@@ -43,13 +43,36 @@ object BillingFixtures {
    * @param ownerEmails        List of billing project owner email addresses. [default: empty]
    * @param userEmails         List of billing project user email addresses [default: empty]
    * @param testCode           Code to exercise with new billing project
-   * @param ownerAuthToken     Auth token of billing project creator
+   * @param creatorAuthToken   Auth token of billing project creator
    */
   def withCleanBillingProjectF[F[_], A](billingAccountName: String,
                                         projectNamePrefix: String = "tmp-billing-project-",
                                         ownerEmails: List[String] = List.empty,
                                         userEmails: List[String] = List.empty
-  )(testCode: String => F[A])(implicit ownerAuthToken: AuthToken, F: Sync[F]): F[A] = {
+  )(testCode: String => F[A])(implicit creatorAuthToken: AuthToken, F: Sync[F]): F[A] = {
+    def addMembers(projectName: String, emails: List[String], role: BillingProjectRole): F[Unit] =
+      emails.traverse_ { email =>
+        F.delay(Orchestration.billingV2.addUserToBillingProject(projectName, email, role))
+      }
+
+    BillingProject.resource(billingAccountName, projectNamePrefix).use { projectName =>
+      addMembers(projectName, ownerEmails, BillingProjectRole.Owner) *>
+        addMembers(projectName, userEmails, BillingProjectRole.User) *>
+        testCode(projectName)
+    }
+  }
+
+  /**
+   * Create a v2 billing project `Resource`.
+   *
+   * @param billingAccountName Name of Google billing account the new billing project will bill to.
+   * @param projectNamePrefix  Prefix for billing project name. [default: "tmp-billing-project-"]
+   * @param creatorAuthToken   Auth token of billing project creator
+   */
+  def resource[F[_]](billingAccountName: String, projectNamePrefix: String = "tmp-billing-project-")(implicit
+    creatorAuthToken: AuthToken,
+    F: Sync[F]
+  ): Resource[F, String] = {
     def createProject: F[String] = F.delay {
       val projectName = projectNamePrefix ++ UUID.randomUUID.toString.replace("-", "")
       Orchestration.billingV2.createBillingProject(projectName, billingAccountName)
@@ -59,15 +82,6 @@ object BillingFixtures {
     def destroyProject(projectName: String): F[Unit] =
       F.delay(Orchestration.billingV2.deleteBillingProject(projectName)) *> F.unit
 
-    def addMembers(projectName: String, emails: List[String], role: BillingProjectRole): F[Unit] =
-      emails.traverse_ { email =>
-        F.delay(Orchestration.billingV2.addUserToBillingProject(projectName, email, role))
-      }
-
-    Resource.make(createProject)(destroyProject).use { projectName =>
-      addMembers(projectName, ownerEmails, BillingProjectRole.Owner) *>
-        addMembers(projectName, userEmails, BillingProjectRole.User) *>
-        testCode(projectName)
-    }
+    Resource.make(createProject)(destroyProject)
   }
 }
