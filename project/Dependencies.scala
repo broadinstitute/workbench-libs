896aae929b1d3d2ecdab97b9331fc6394a183926
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-06 20:38:52 +0200

    Update google-cloud-storage from 2.2.2 to 2.2.3 (#884)
    * Update google-cloud-storage from 2.2.2 to 2.2.3
    
    * Revert commit(s) 435db899
    
    * Update google-cloud-storage from 2.2.2 to 2.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
 
   val googleRpc2: ModuleID =               "io.grpc" % "grpc-core" % "1.43.1"
   val googleFirestore: ModuleID = "com.google.cloud" % "google-cloud-firestore" % "2.6.2"
-  val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "2.2.2"
+  val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "2.2.3"
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.123.25" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.116.3"
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.3.0"
