088fc10e16ea3fc9f150671f6ac4d7b07f5ccf01
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-06 20:35:47 +0200

    Update google-cloud-pubsub from 1.115.5 to 1.116.3 (#953)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,7 +63,7 @@ object Dependencies {
   val googleFirestore: ModuleID = "com.google.cloud" % "google-cloud-firestore" % "2.6.2"
   val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "2.2.2"
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.123.25" % "test"
-  val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.115.5"
+  val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.116.3"
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.3.0"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.8.0"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "3.0.3"
