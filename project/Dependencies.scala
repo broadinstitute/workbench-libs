f33b446b4202dc1277fcd8d21760276e417e5dd3
Scala Steward <me@scala-steward.org>
2022-05-04 09:15:10 +0200

    Update client-java from 15.0.0 to 15.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -69,7 +69,7 @@ object Dependencies {
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.8.1"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "3.0.3"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.3.6"
-  val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "15.0.0"
+  val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "15.0.1"
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "2.6.0"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "2.1.11"
   val googleStorageTransferService: ModuleID = "com.google.cloud" % "google-cloud-storage-transfer" % "1.0.4"
