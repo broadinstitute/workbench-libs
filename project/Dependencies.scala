db7bec0cf8f433e10215176b66a560618e639221
Scala Steward <me@scala-steward.org>
2022-05-07 00:08:26 +0200

    Revert commit(s) 61588b89

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
   val googleGuava: ModuleID = "com.google.guava"  % "guava" % "31.1-jre"
   val googleRpc: ModuleID =               "io.grpc" % "grpc-core" % "1.34.0"
 
-  val googleRpc2: ModuleID =               "io.grpc" % "grpc-core" % "1.46.0"
+  val googleRpc2: ModuleID =               "io.grpc" % "grpc-core" % "1.43.2"
   val googleFirestore: ModuleID = "com.google.cloud" % "google-cloud-firestore" % "3.0.21"
   val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "2.2.3"
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.123.27" % "test"
