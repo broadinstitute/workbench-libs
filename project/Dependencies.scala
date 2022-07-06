9c1f70aaaf83688f0d02cb1bb224c2420b9c9112
Scala Steward <scala_steward@virtuslab.com>
2022-07-06 13:11:39 +0000

    Update google-cloud-container from 2.3.7 to 2.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.3.1"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.9.1"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "3.0.4"
-  val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.3.7"
+  val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.4.1"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "15.0.1"
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "2.6.2"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "2.1.12"
