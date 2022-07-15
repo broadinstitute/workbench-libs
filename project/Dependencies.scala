7c71a9c2cf03d5dd0e551b218f6a0bb6cd7a1f7c
Scala Steward <scala_steward@virtuslab.com>
2022-07-15 13:05:37 +0000

    Update google-cloud-resourcemanager from 1.2.11 to 1.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "2.6.2"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "2.1.12"
   val googleStorageTransferService: ModuleID = "com.google.cloud" % "google-cloud-storage-transfer" % "1.2.0"
-  val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "1.2.11"
+  val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "1.5.0"
   //the below v1 module is a dependency for v2 because it contains the OAuth scopes necessary to created scoped credentials
   val googleContainerV1: ModuleID = "com.google.apis" % "google-api-services-container" % "v1-rev20220419-1.32.1"
 
