891b768207153fe7bb651f67b6908edc26ef37b6
Justin Canas <justincanas16@gmail.com>
2022-08-03 12:20:04 -0700

    [IA-3514] sas token used over conn string (#1053)
    * sas token used over conn string
    "
    
    * travis
    
    * cleanup
    
    * cleanup
    
    * feedback
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureStorageService.scala
+++ azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureStorageService.scala
@@ -4,53 +4,56 @@ import java.nio.file.Path
 
 import cats.effect.{Async, Resource}
 import cats.mtl.Ask
+import com.azure.storage.blob.{BlobServiceClient, BlobServiceClientBuilder}
 import com.azure.storage.blob.models.{BlobItem, ListBlobsOptions}
 import fs2.{Pipe, Stream}
 import org.broadinstitute.dsde.workbench.model.TraceId
 import org.typelevel.log4cats.StructuredLogger
+import cats.implicits._
 
 import scala.concurrent.duration.FiniteDuration
 
 trait AzureStorageService[F[_]] {
-  def listObjects(connectionString: ConnectionString, containerName: ContainerName, opts: Option[ListBlobsOptions])(
-    implicit ev: Ask[F, TraceId]
+  def listObjects(containerName: ContainerName, opts: Option[ListBlobsOptions])(implicit
+    ev: Ask[F, TraceId]
   ): Stream[F, BlobItem]
 
-  def uploadBlob(connectionString: ConnectionString, containerName: ContainerName, blobName: BlobName)(implicit
+  def uploadBlob(containerName: ContainerName, blobName: BlobName)(implicit
     ev: Ask[F, TraceId]
   ): Pipe[F, Byte, Unit]
 
-  // The file cannot exist
-  def downloadBlob(connectionString: ConnectionString,
-                   containerName: ContainerName,
-                   blobName: BlobName,
-                   path: Path,
-                   overwrite: Boolean
-  )(implicit
+  def downloadBlob(containerName: ContainerName, blobName: BlobName, path: Path, overwrite: Boolean)(implicit
     ev: Ask[F, TraceId]
   ): F[Unit]
 
-  def getBlob(connectionString: ConnectionString, containerName: ContainerName, blobName: BlobName)(implicit
+  def getBlob(containerName: ContainerName, blobName: BlobName)(implicit
     ev: Ask[F, TraceId]
   ): Stream[F, Byte]
 
-  def createContainer(connectionString: ConnectionString, containerName: ContainerName)(implicit
-    ev: Ask[F, TraceId]
-  ): F[Unit]
-
-  def deleteContainer(connectionString: ConnectionString, containerName: ContainerName)(implicit
+  def deleteBlob(containerName: ContainerName, blobName: BlobName)(implicit
     ev: Ask[F, TraceId]
   ): F[Unit]
 }
 
 object AzureStorageService {
-  def fromAzureAppRegistrationConfig[F[_]: Async: StructuredLogger](
+  def fromSasToken[F[_]: Async: StructuredLogger](
     azureStorageConfig: AzureStorageConfig
   ): Resource[F, AzureStorageService[F]] =
-    Resource.eval(Async[F].pure(new AzureStorageInterp(azureStorageConfig)))
+    for {
+      storageServiceClient <- Resource.eval(
+        Async[F].delay(
+          new BlobServiceClientBuilder()
+            .sasToken(azureStorageConfig.sasToken.value)
+            .endpoint(azureStorageConfig.endpointUrl.value)
+            .buildClient()
+        )
+      )
+    } yield new AzureStorageInterp(azureStorageConfig, storageServiceClient)
 }
 
-final case class AzureStorageConfig(listTimeout: FiniteDuration)
+final case class AzureStorageConfig(listTimeout: FiniteDuration, sasToken: SasToken, endpointUrl: EndpointUrl)
+final case class SasToken(value: String) extends AnyVal
+final case class EndpointUrl(value: String) extends AnyVal
 
 // See this article for more information on connection strings: https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-java?tabs=powershell%2Cenvironment-variable-windows#get-the-connection-string
 final case class ConnectionString(value: String) extends AnyVal
