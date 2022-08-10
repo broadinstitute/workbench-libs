1f16cf5a07c2c735d7c6c85128570e3250bf070e
Qi Wang <32771737+Qi77Qi@users.noreply.github.com>
2022-08-10 16:08:47 -0400

    remove firestore (#1072)

--- google2/src/main/scala/org/broadinstitute/dsde/workbench/google2/util/DistributedLockAlgebra.scala
+++
@@ -1,8 +0,0 @@
-package org.broadinstitute.dsde.workbench.google2
-package util
-
-import cats.effect.Resource
-
-trait DistributedLockAlgebra[F[_]] {
-  def withLock(lock: LockPath): Resource[F, Unit]
-}
