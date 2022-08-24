0f4b9a198a5ed1ce98a9cf4c31c49d4ce02d1677
Scala Steward <scala_steward@virtuslab.com>
2022-08-24 13:29:38 +0000

    Update google-cloud-kms from 2.3.1 to 2.6.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,7 +63,7 @@ object Dependencies {
   val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "2.9.3"
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.124.13" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.120.11"
-  val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.3.1"
+  val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.6.4"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.11.0"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "4.0.0"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.5.0"
