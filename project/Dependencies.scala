c2cd543fc5f1badd0c2d50b8428839e142cc0840
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-09-15 17:57:15 +0200

    Update akka-http, akka-http-spray-json, ... from 10.2.9 to 10.2.10 (#1111)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val akkaV         = "2.6.19"
-  val akkaHttpV     = "10.2.9"
+  val akkaHttpV     = "10.2.10"
   val jacksonV      = "2.13.3"
   val googleV       = "1.22.0"
   val scalaLoggingV = "3.9.5"
