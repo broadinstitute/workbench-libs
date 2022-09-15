c91682ee23115b82e05f3090a1f2f97a1d5c322e
Scala Steward <scala_steward@virtuslab.com>
2022-09-15 13:08:44 +0000

    Update akka-actor, akka-stream, ... from 2.6.19 to 2.6.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val akkaV         = "2.6.19"
+  val akkaV         = "2.6.20"
   val akkaHttpV     = "10.2.9"
   val jacksonV      = "2.13.3"
   val googleV       = "1.22.0"
