e9e06593d02064b4fda00970af64dea7faa594d2
Scala Steward <scala_steward@virtuslab.com>
2022-09-17 14:29:49 +0000

    Update jackson-module-scala from 2.13.3 to 2.13.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val akkaV         = "2.6.19"
   val akkaHttpV     = "10.2.10"
-  val jacksonV      = "2.13.3"
+  val jacksonV      = "2.13.4"
   val googleV       = "1.22.0"
   val scalaLoggingV = "3.9.5"
   val scalaTestV    = "3.2.13"
