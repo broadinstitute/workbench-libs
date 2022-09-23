075f507810c8870554e5b7f525a24e4bf7c4be62
Nate L <Nate.Landolt@gmail.com>
2022-09-23 09:43:43 -0400

    [IA-3711] Add stop and start functionality (#1098)
    * Add Azure start and stop functionality.

--- azure/src/test/scala/org/broadinstitute/dsde/workbench/azure/mock/FakeAzureVmService.scala
+++ azure/src/test/scala/org/broadinstitute/dsde/workbench/azure/mock/FakeAzureVmService.scala
@@ -8,6 +8,7 @@ import com.azure.resourcemanager.resources.fluentcore.model.Accepted
 import org.broadinstitute.dsde.workbench.azure.AzureVmService
 import org.broadinstitute.dsde.workbench.model.TraceId
 import org.broadinstitute.dsde.workbench.util2.InstanceName
+import reactor.core.publisher.Mono
 
 class FakeAzureVmService extends AzureVmService[IO] {
   override def getAzureVm(name: InstanceName, cloudContext: AzureCloudContext)(implicit
@@ -17,6 +18,14 @@ class FakeAzureVmService extends AzureVmService[IO] {
   override def deleteAzureVm(name: InstanceName, cloudContext: AzureCloudContext, forceDeletion: Boolean)(implicit
     ev: Ask[IO, TraceId]
   ): IO[Option[Accepted[Void]]] = IO.pure(None)
+
+  override def startAzureVm(name: InstanceName, cloudContext: AzureCloudContext)(implicit
+    ev: Ask[IO, TraceId]
+  ): IO[Option[Mono[Void]]] = IO.pure(None)
+
+  override def stopAzureVm(name: InstanceName, cloudContext: AzureCloudContext)(implicit
+    ev: Ask[IO, TraceId]
+  ): IO[Option[Mono[Void]]] = IO.pure(None)
 }
 
 object FakeAzureVmService extends FakeAzureVmService
