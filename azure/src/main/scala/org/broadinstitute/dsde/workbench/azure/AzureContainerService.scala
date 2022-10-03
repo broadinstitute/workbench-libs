c0d9a5563790e06853a6a0d65640b1580ecb77d7
Robert Title <rtitle@broadinstitute.org>
2022-10-03 13:42:38 -0400

    Initial AKS client, support for authentication

---
+++ azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureContainerService.scala
@@ -0,0 +1,26 @@
+package org.broadinstitute.dsde.workbench.azure
+
+import cats.effect.{Async, Resource}
+import cats.mtl.Ask
+import com.azure.identity.ClientSecretCredentialBuilder
+import org.broadinstitute.dsde.workbench.model.TraceId
+import org.typelevel.log4cats.StructuredLogger
+
+trait AzureContainerService[F[_]] {
+  def getCredentials(name: AKSClusterName, cloudContext: AzureCloudContext)(implicit
+    ev: Ask[F, TraceId]
+  ): F[AKSCredentials]
+}
+
+object AzureContainerService {
+  def fromAzureAppRegistrationConfig[F[_]: Async: StructuredLogger](
+    azureAppRegistrationConfig: AzureAppRegistrationConfig
+  ): Resource[F, AzureContainerService[F]] = {
+    val clientSecretCredential = new ClientSecretCredentialBuilder()
+      .clientId(azureAppRegistrationConfig.clientId.value)
+      .clientSecret(azureAppRegistrationConfig.clientSecret.value)
+      .tenantId(azureAppRegistrationConfig.managedAppTenantId.value)
+      .build
+    Resource.eval(Async[F].pure(new AzureContainerServiceInterp(clientSecretCredential)))
+  }
+}
---
+++ azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureContainerServiceInterp.scala
@@ -0,0 +1,61 @@
+package org.broadinstitute.dsde.workbench.azure
+
+import cats.effect.Async
+import cats.mtl.Ask
+import cats.syntax.all._
+import com.azure.core.management.AzureEnvironment
+import com.azure.core.management.profile.AzureProfile
+import com.azure.identity.ClientSecretCredential
+import com.azure.resourcemanager.containerservice.ContainerServiceManager
+import io.kubernetes.client.util.KubeConfig
+import org.broadinstitute.dsde.workbench.model.{TraceId, WorkbenchException}
+import org.broadinstitute.dsde.workbench.util2.tracedLogging
+import org.typelevel.log4cats.StructuredLogger
+
+import java.io.{ByteArrayInputStream, InputStreamReader}
+import scala.jdk.CollectionConverters._
+
+class AzureContainerServiceInterp[F[_]](clientSecretCredential: ClientSecretCredential)(implicit
+  val F: Async[F],
+  logger: StructuredLogger[F]
+) extends AzureContainerService[F] {
+  override def getCredentials(name: AKSClusterName, cloudContext: AzureCloudContext)(implicit
+    ev: Ask[F, TraceId]
+  ): F[AKSCredentials] =
+    for {
+      mgr <- buildContainerServiceManager(cloudContext)
+      resp <- tracedLogging(
+        F.delay(
+          mgr
+            .kubernetesClusters()
+            .manager()
+            .serviceClient()
+            .getManagedClusters()
+            .listClusterMonitoringUserCredentials(cloudContext.managedResourceGroupName.value, name.value)
+        ),
+        s"com.azure.resourcemanager.containerservice.fluent.ManagedClustersClient.listClusterMonitoringUserCredentials(${cloudContext.managedResourceGroupName.value}, ${name})"
+      )
+      clusterCredential <- F.fromOption(resp.kubeconfigs().asScala.headOption,
+                                        new WorkbenchException("No AKS credential")
+      )
+      // Parse the kubeconfig file
+      kubeConfig <- F.delay(
+        KubeConfig.loadKubeConfig(new InputStreamReader(new ByteArrayInputStream(clusterCredential.value)))
+      )
+      // Null-check fields from the Java API
+      server <- F.fromOption(Option(kubeConfig.getServer).map(AKSServer), new WorkbenchException("No AKS server"))
+      userToken <- F.fromOption(kubeConfig.getCredentials.asScala.get("token").map(AKSToken),
+                                new WorkbenchException("No AKS token")
+      )
+      certificate <- F.fromOption(Option(kubeConfig.getCertificateAuthorityData).map(AKSCertificate),
+                                  new WorkbenchException("No AKS certificate")
+      )
+
+    } yield AKSCredentials(server, userToken, certificate)
+
+  private def buildContainerServiceManager(cloudContext: AzureCloudContext): F[ContainerServiceManager] = {
+    val azureProfile =
+      new AzureProfile(cloudContext.tenantId.value, cloudContext.subscriptionId.value, AzureEnvironment.AZURE)
+    F.delay(ContainerServiceManager.authenticate(clientSecretCredential, azureProfile))
+  }
+}
