8aa3974d85ce86f57c7e29f0fcf5b79ae6b062a8
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 14:29:56 +0000

    Update google-cloud-compute from 1.12.0 to 1.12.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -62,7 +62,7 @@ object Dependencies {
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.124.14" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.120.15"
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.6.4"
-  val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.12.0"
+  val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.12.1"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "4.0.4"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.5.2"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "16.0.0"
