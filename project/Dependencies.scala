3acd1e8a0205fda2fadcb6ff322bef4b55ae32c0
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 14:29:46 +0000

    Update google-cloud-billing from 2.3.0 to 2.3.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.5.2"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "16.0.0"
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "2.16.0"
-  val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "2.3.0"
+  val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "2.3.5"
   val googleStorageTransferService: ModuleID = "com.google.cloud" % "google-cloud-storage-transfer" % "1.2.2"
   val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "1.5.4"
   //the below v1 module is a dependency for v2 because it contains the OAuth scopes necessary to created scoped credentials
