a29eb1ec58203d03def8d95471305b4ec0ce4f46
Scala Steward <scala_steward@virtuslab.com>
2022-10-17 14:28:57 +0000

    Update azure-storage-blob from 12.19.1 to 12.20.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -105,7 +105,7 @@ object Dependencies {
   val azureResourceManagerCompute = "com.azure.resourcemanager" % "azure-resourcemanager-compute" % "2.17.0"
   val azureIdentity =  "com.azure" % "azure-identity" % "1.5.5"
   val azureRelay =     "com.azure.resourcemanager" % "azure-resourcemanager-relay" % "1.0.0-beta.2"
-  val azureStorageBlob =  "com.azure" % "azure-storage-blob" % "12.19.1"
+  val azureStorageBlob =  "com.azure" % "azure-storage-blob" % "12.20.0"
   val azureResourceManagerContainerService = "com.azure.resourcemanager" % "azure-resourcemanager-containerservice" % "2.19.0"
 
   val commonDependencies = Seq(
