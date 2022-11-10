4d13739f0620c7c9e72b806f4328d771159ffd23
Tristan Garwood <tristan.garwood@gmail.com>
2022-11-10 16:09:33 -0500

    TOAZ-230 Implement list pod statuses and delete namespace for azure container service.

--- azure/src/test/scala/org/broadinstitute/dsde/workbench/azure/mock/FakeAzureContainerService.scala
+++ azure/src/test/scala/org/broadinstitute/dsde/workbench/azure/mock/FakeAzureContainerService.scala
@@ -3,15 +3,8 @@ package org.broadinstitute.dsde.workbench.azure.mock
 import cats.effect.IO
 import cats.mtl.Ask
 import com.azure.resourcemanager.containerservice.models.KubernetesCluster
-import org.broadinstitute.dsde.workbench.azure.{
-  AKSCertificate,
-  AKSClusterName,
-  AKSCredentials,
-  AKSServer,
-  AKSToken,
-  AzureCloudContext,
-  AzureContainerService
-}
+import io.kubernetes.client.openapi.models.{V1Pod, V1Status}
+import org.broadinstitute.dsde.workbench.azure.{AKSCertificate, AKSClusterName, AKSCredentials, AKSServer, AKSToken, AzureCloudContext, AzureContainerService}
 import org.broadinstitute.dsde.workbench.model.TraceId
 
 class FakeAzureContainerService extends AzureContainerService[IO] {
@@ -22,5 +15,13 @@ class FakeAzureContainerService extends AzureContainerService[IO] {
   override def getCluster(name: AKSClusterName, cloudContext: AzureCloudContext)(implicit
     ev: Ask[IO, TraceId]
   ): IO[KubernetesCluster] =
+    IO.raiseError(new NotImplementedError())
+
+  /** Lists pods in a AKS cluster's namespace. */
+  override def listNamespacePods(name: AKSClusterName, namespace: String, cloudContext: AzureCloudContext)(implicit ev: Ask[IO, TraceId]): IO[List[V1Pod]] =
+    IO.raiseError(new NotImplementedError())
+
+  /** Deletes a AKS namespace. */
+  override def deleteNamespace(name: AKSClusterName, namespace: String, cloudContext: AzureCloudContext)(implicit ev: Ask[IO, TraceId]): IO[V1Status] =
     IO.raiseError(new NotImplementedError())
 }
