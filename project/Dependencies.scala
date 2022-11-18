aa582a298c35f6e2a849a0c6c393bcc00277b5f9
Scala Steward <scala_steward@virtuslab.com>
2022-11-18 15:27:22 +0000

    Update google-cloud-nio from 0.124.19 to 0.124.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies {
 
   val googleRpc2: ModuleID =               "io.grpc" % "grpc-core" % "1.51.0"
   val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "2.14.0"
-  val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.124.19" % "test"
+  val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.124.20" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.120.24"
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.6.4"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.12.0"
