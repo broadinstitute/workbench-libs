a52046bc462c83c898f70feaee0671fec26ef6b6
Scala Steward <scala_steward@virtuslab.com>
2023-02-16 14:35:38 +0000

    Update cats-effect from 3.4.4 to 3.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
   val bouncyCastleProviderExt: ModuleID = "org.bouncycastle" % "bcprov-ext-jdk15on" % bouncyCastleVersion
   val bouncyCastleProvider: ModuleID = "org.bouncycastle" % "bcprov-jdk15on" % bouncyCastleVersion
 
-  val catsEffect: ModuleID = "org.typelevel" %% "cats-effect" % "3.4.4"
+  val catsEffect: ModuleID = "org.typelevel" %% "cats-effect" % "3.4.7"
 
   // metrics-scala transitively pulls in io.dropwizard.metrics:metrics-core
   val metricsScala: ModuleID =      "nl.grons"              %% "metrics4-scala"    % "4.2.9"
