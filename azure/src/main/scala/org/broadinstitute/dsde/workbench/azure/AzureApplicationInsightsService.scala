4910768849f08b45d349237f28fe60eaf0b08930
n8landolt <nlandolt@broadinstitute.org>
2023-02-21 16:53:05 -0500

    Added get list of application insights.

---
+++ azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureApplicationInsightsService.scala
@@ -0,0 +1,30 @@
+package org.broadinstitute.dsde.workbench.azure
+
+import cats.effect.{Async, Resource}
+import cats.mtl.Ask
+import com.azure.core.http.rest.PagedIterable
+import com.azure.identity.ClientSecretCredentialBuilder
+import com.azure.resourcemanager.applicationinsights.models.ApplicationInsightsComponent
+import org.broadinstitute.dsde.workbench.model.TraceId
+import org.typelevel.log4cats.StructuredLogger
+
+trait AzureApplicationInsightsService[F[_]] {
+
+  def getApplicationInsights(cloudContext: AzureCloudContext)(implicit
+    ev: Ask[F, TraceId]
+  ): F[PagedIterable[ApplicationInsightsComponent]]
+
+}
+
+object AzureApplicationInsightsService {
+  def fromAzureAppRegistrationConfig[F[_]: Async: StructuredLogger](
+    azureAppRegistrationConfig: AzureAppRegistrationConfig
+  ): Resource[F, AzureApplicationInsightsService[F]] = {
+    val clientSecretCredential = new ClientSecretCredentialBuilder()
+      .clientId(azureAppRegistrationConfig.clientId.value)
+      .clientSecret(azureAppRegistrationConfig.clientSecret.value)
+      .tenantId(azureAppRegistrationConfig.managedAppTenantId.value)
+      .build
+    Resource.eval(Async[F].pure(new AzureApplicationInsightsServiceInterp(clientSecretCredential)))
+  }
+}
---
+++ azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureApplicationInsightsServiceInterp.scala
@@ -0,0 +1,39 @@
+package org.broadinstitute.dsde.workbench.azure
+
+import cats.effect.Async
+import cats.mtl.Ask
+import cats.syntax.all._
+import com.azure.core.http.rest.PagedIterable
+import com.azure.core.management.AzureEnvironment
+import com.azure.core.management.profile.AzureProfile
+import com.azure.identity.ClientSecretCredential
+import com.azure.resourcemanager.applicationinsights.ApplicationInsightsManager
+import com.azure.resourcemanager.applicationinsights.models.ApplicationInsightsComponent
+import org.broadinstitute.dsde.workbench.model.TraceId
+import org.broadinstitute.dsde.workbench.util2.tracedLogging
+import org.typelevel.log4cats.StructuredLogger
+
+class AzureApplicationInsightsServiceInterp[F[_]](clientSecretCredential: ClientSecretCredential)(implicit
+  val F: Async[F],
+  logger: StructuredLogger[F]
+) extends AzureApplicationInsightsService[F] {
+
+  override def getApplicationInsights(cloudContext: AzureCloudContext)(implicit
+    ev: Ask[F, TraceId]
+  ): F[PagedIterable[ApplicationInsightsComponent]] =
+    for {
+      mgr <- buildApplicationInsightsManager(cloudContext)
+      resp <- tracedLogging(
+        F.delay(
+          mgr.components().listByResourceGroup(cloudContext.managedResourceGroupName.value)
+        ),
+        s"com.azure.resourcemanager.applicationinsights,listByResourceGroup(${cloudContext.managedResourceGroupName.value})"
+      )
+    } yield resp
+
+  private def buildApplicationInsightsManager(cloudContext: AzureCloudContext): F[ApplicationInsightsManager] = {
+    val azureProfile =
+      new AzureProfile(cloudContext.tenantId.value, cloudContext.subscriptionId.value, AzureEnvironment.AZURE)
+    F.delay(ApplicationInsightsManager.authenticate(clientSecretCredential, azureProfile))
+  }
+}
