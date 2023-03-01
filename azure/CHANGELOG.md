b328f135e200919b702aa5069f5cb1fac2b51a85
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2023-03-01 10:18:55 -0500

    IA-4017 Add AzureBatchService (#1268)
    * Add AzureBatchService
    
    * scalafmt
    
    * Ask Travis to replace hashes for us, adjust changelog
    
    * Fix log message

--- azure/CHANGELOG.md
+++ azure/CHANGELOG.md
@@ -2,11 +2,18 @@
 
 This file documents changes to the `workbench-azure` library, including notes on how to upgrade to new versions.
 
+latest SBT dependency: `"org.broadinstitute.dsde.workbench" %% "workbench-azure" % "0.2-TRAVIS-REPLACE-ME"`
+
 ## 0.2
 
 ### Changed
 
 - AzureVmService now calls deallocateAsync instead of powerOffAsync when pausing an Azure virtual machine.
+
+### Added
+
+- Added AzureApplicationInsightsService
+- Added AzureBatchService
 
 ## 0.1
 
@@ -14,6 +21,4 @@ This file documents changes to the `workbench-azure` library, including notes on
 
 - Added AzureStorageService and AzureStorageManualTest
 - Added AzureContainerService#listClusters
-- Added AzureApplicationInsightsService
 
-latest SBT dependency: `"org.broadinstitute.dsde.workbench" %% "workbench-azure" % "0.2-d0369c0"`
---
+++ azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureBatchServiceInterp.scala
@@ -0,0 +1,38 @@
+package org.broadinstitute.dsde.workbench.azure
+
+import cats.effect.Async
+import cats.mtl.Ask
+import cats.syntax.all._
+import com.azure.core.management.AzureEnvironment
+import com.azure.core.management.profile.AzureProfile
+import com.azure.identity.ClientSecretCredential
+import com.azure.resourcemanager.batch.BatchManager
+import com.azure.resourcemanager.batch.models.BatchAccount
+import org.broadinstitute.dsde.workbench.model.TraceId
+import org.broadinstitute.dsde.workbench.util2.tracedLogging
+import org.typelevel.log4cats.StructuredLogger
+
+class AzureBatchServiceInterp[F[_]](clientSecretCredential: ClientSecretCredential)(implicit
+  val F: Async[F],
+  logger: StructuredLogger[F]
+) extends AzureBatchService[F] {
+
+  override def getBatchAccount(name: BatchAccountName, cloudContext: AzureCloudContext)(implicit
+    ev: Ask[F, TraceId]
+  ): F[BatchAccount] =
+    for {
+      mgr <- buildBatchManager(cloudContext)
+      resp <- tracedLogging(
+        F.delay(
+          mgr.batchAccounts().getByResourceGroup(cloudContext.managedResourceGroupName.value, name.value)
+        ),
+        s"com.azure.resourcemanager.batch,getByResourceGroup(${cloudContext.managedResourceGroupName.value}, ${name.value})"
+      )
+    } yield resp
+
+  private def buildBatchManager(cloudContext: AzureCloudContext): F[BatchManager] = {
+    val azureProfile =
+      new AzureProfile(cloudContext.tenantId.value, cloudContext.subscriptionId.value, AzureEnvironment.AZURE)
+    F.delay(BatchManager.authenticate(clientSecretCredential, azureProfile))
+  }
+}
