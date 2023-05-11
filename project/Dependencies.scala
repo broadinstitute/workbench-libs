4ab27d4bef67d6956c494473d52de0548805bab5
tlangs <tlangs@broadinstitute.org>
2023-05-11 12:45:15 -0400

    ID-613 Remediate CVE-2022-45688

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "4.10.0"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.16.0"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "17.0.1"
-  val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "2.20.2"
+  val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "2.23.2"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "2.3.0"
   val googleStorageTransferService: ModuleID = "com.google.cloud" % "google-cloud-storage-transfer" % "1.13.0"
   val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "1.5.4"
