65965eaeed59b1ce22804e059480f9e27e7fd31d
Scala Steward <scala_steward@virtuslab.com>
2023-06-06 13:45:57 +0000

    Update azure-identity from 1.7.3 to 1.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -106,7 +106,7 @@ object Dependencies {
   val swaggerUi = "org.webjars" % "swagger-ui" % "4.11.1"
 
   val azureResourceManagerCompute = "com.azure.resourcemanager" % "azure-resourcemanager-compute" % "2.26.0" exclude("net.minidev", "json-smart")
-  val azureIdentity =  "com.azure" % "azure-identity" % "1.7.3"
+  val azureIdentity =  "com.azure" % "azure-identity" % "1.9.1"
   val azureRelay =     "com.azure.resourcemanager" % "azure-resourcemanager-relay" % "1.0.0-beta.2"
   val azureStorageBlob =  "com.azure" % "azure-storage-blob" % "12.22.0"
   val azureResourceManagerContainerService = "com.azure.resourcemanager" % "azure-resourcemanager-containerservice" % "2.26.0"
