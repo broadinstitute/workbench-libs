ddda637bdea022d7a9f3cc6b6a3f9dff4dec8db8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-08-15 17:34:15 +0200

    Update google-cloud-kms from 2.6.8 to 2.26.0 (#1456)
    Co-authored-by: LizBaldo <LizBaldo@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
   val googleStorageNew: ModuleID = "com.google.cloud" % "google-cloud-storage" % "2.20.2"
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.126.19" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.123.15"
-  val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.6.8"
+  val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.26.0"
   val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.12.0"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "4.26.0"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.26.0"
