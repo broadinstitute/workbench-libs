8668b168d1dbb6c6ca7a1e139b8f5d5e316efe9f
Blake Geno <bgeno@broadinstitute.org>
2023-08-21 11:35:58 -0400

    remove billing endpoints from profile object

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/Orchestration.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/Orchestration.scala
@@ -667,12 +667,7 @@ trait Orchestration extends RestClient with LazyLogging with SprayJsonSupport wi
 
     def getUser()(implicit token: AuthToken): Map[String, String] =
       parseResponseAs[Map[String, String]](getRequest(apiUrl(s"register/profile")))
-
-    def getUserBillingProjects()(implicit token: AuthToken): List[Map[String, String]] =
-      parseResponseAs[List[Map[String, String]]](getRequest(apiUrl(s"api/profile/billing")))
-
-    def getUserBillingProjectStatus(projectName: String)(implicit token: AuthToken): Map[String, String] =
-      parseResponseAs[Map[String, String]](getRequest(apiUrl(s"api/profile/billing/$projectName")))
+    
   }
 
   def importMetaData(ns: String, wsName: String, fileName: String, fileContent: String)(implicit
