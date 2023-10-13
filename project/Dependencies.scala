28735a5bedb2c6ebd73a615813e51edc7e938f40
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-10-13 23:16:04 +0200

    Update google-cloud-compute from 1.34.0 to 1.37.0 (#1543)
    * Update google-cloud-compute from 1.33.0 to 1.37.0
    
    * Revert commit(s) c6d3e1e4
    
    * Update google-cloud-compute from 1.34.0 to 1.37.0
    
    ---------
    
    Co-authored-by: lmcnatt <85642387+lucymcnatt@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
   val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.127.4" % "test"
   val googlePubsubNew: ModuleID = "com.google.cloud" % "google-cloud-pubsub" % "1.125.5"
   val googleKms: ModuleID = "com.google.cloud" % "google-cloud-kms" % "2.27.0"
-  val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.34.0"
+  val googleComputeNew: ModuleID = "com.google.cloud" % "google-cloud-compute" % "1.37.0"
   val googleDataproc: ModuleID =    "com.google.cloud" % "google-cloud-dataproc" % "4.24.0"
   val googleContainer: ModuleID = "com.google.cloud" % "google-cloud-container" % "2.30.0"
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "18.0.1"
