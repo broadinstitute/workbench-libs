fd57110ba0ad5115ef63965a051fd6269180e39a
Scala Steward <scala_steward@virtuslab.com>
2023-10-24 15:01:30 +0000

    Update google-cloud-storage-transfer from 1.24.0 to 1.28.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
   val kubernetesClient: ModuleID = "io.kubernetes" % "client-java" % "18.0.1"
   val googleBigQueryNew: ModuleID = "com.google.cloud" % "google-cloud-bigquery" % "2.33.1"
   val google2CloudBilling = "com.google.cloud" % "google-cloud-billing" % "2.27.0"
-  val googleStorageTransferService: ModuleID = "com.google.cloud" % "google-cloud-storage-transfer" % "1.24.0"
+  val googleStorageTransferService: ModuleID = "com.google.cloud" % "google-cloud-storage-transfer" % "1.28.0"
   val googleResourceManager =  "com.google.cloud" % "google-cloud-resourcemanager" % "1.26.0"
   //the below v1 module is a dependency for v2 because it contains the OAuth scopes necessary to created scoped credentials
   val googleContainerV1: ModuleID = "com.google.apis" % "google-api-services-container" % "v1-rev20230919-2.0.0"
