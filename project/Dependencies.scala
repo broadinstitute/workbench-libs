4ea5341983bbd85d19eef6b38245a46fd8a8f0d0
Cindy Mottershead <cmotters@broadinstitute.org>
2023-12-04 07:57:28 -0500

    remove comment

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val scalaTestV    = "3.2.17"
 
   // TODO upgrade to stable 14.x or 15.0 once that includes a fix to https://github.com/circe/circe-yaml/issues/356
-  val circeVersion = "0.15.0-M1" // "0.14.5"
+  val circeVersion = "0.15.0-M1"
   val http4sVersion = "1.0.0-M38"
   val bouncyCastleVersion = "1.76"
   val openCensusV = "0.31.1"
