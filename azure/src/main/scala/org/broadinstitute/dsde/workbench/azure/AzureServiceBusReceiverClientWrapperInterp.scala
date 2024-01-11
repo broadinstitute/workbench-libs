9f8a8b82b6f6c35de4d74c516ee455ec3f3c580e
Jesus Aguilar <3589801+giventocode@users.noreply.github.com>
2024-01-11 15:38:43 -0500

    in-prog changes

--- azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureServiceBusReceiverClientWrapperInterp.scala
+++ azure/src/main/scala/org/broadinstitute/dsde/workbench/azure/AzureServiceBusReceiverClientWrapperInterp.scala
@@ -1,9 +1,16 @@
 package org.broadinstitute.dsde.workbench.azure
 
-import com.azure.messaging.servicebus.{ServiceBusClientBuilder, ServiceBusReceivedMessage, ServiceBusReceiverAsyncClient}
+import cats.effect.IO
+import com.azure.messaging.servicebus.{ServiceBusClientBuilder, ServiceBusErrorContext, ServiceBusProcessorClient, ServiceBusReceivedMessage, ServiceBusReceivedMessageContext, ServiceBusReceiverAsyncClient}
 import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
+
+import java.util.concurrent.Executors
 
 class AzureServiceBusReceiverClientWrapperInterp(subscriberConfig: AzureServiceBusSubscriberConfig) extends AzureServiceBusReceiverClientWrapper {
+
+  private val executor = Executors.newScheduledThreadPool(10)
+  private val scheduler = Schedulers.fromExecutor(executor)
 
   private val receiverClient: ServiceBusReceiverAsyncClient = new ServiceBusClientBuilder()
     .connectionString(subscriberConfig.connectionString.getOrElse(throw new Exception("Connection string not provided")))
@@ -12,12 +19,40 @@ class AzureServiceBusReceiverClientWrapperInterp(subscriberConfig: AzureServiceB
     .subscriptionName(subscriberConfig.subscriptionName)
     .buildAsyncClient()
 
+  private val processor : ServiceBusProcessorClient = new ServiceBusClientBuilder()
+    .connectionString(subscriberConfig.connectionString.getOrElse(throw new Exception("Connection string not provided")))
+    .processor()
+    .topicName(subscriberConfig.topicName)
+    .processMessage(handleMessage)
+    .processError(handleError)
+    .subscriptionName(subscriberConfig.subscriptionName)
+
+    .buildProcessorClient()
+
+  private def handleMessage(context: ServiceBusReceivedMessageContext): Unit = {
+      println(s"Received message: ${context.getMessage.getBody}")
+  }
+
+  private def handleError(context: ServiceBusErrorContext): Unit = {
+      println(s"Error when receiving messages: ${context.getException }")
+  }
+
+  def startProcessor(): Unit = {
+    processor.start()
+  }
+
+  def stopProcessor(): Unit = {
+    processor.stop()
+  }
+
   override def close(): Unit = {
     receiverClient.close()
+    executor.shutdown()
   }
 
   override def receiveMessagesAsync(): Flux[ServiceBusReceivedMessage] = {
     receiverClient.receiveMessages()
+      .subscribeOn(scheduler)
   }
 
   override def complete(message: ServiceBusReceivedMessage): Unit = {
--- azure/src/test/scala/org/broadinstitute/dsde/workbench/azure/AzureSubscriberSpec.scala
+++ azure/src/test/scala/org/broadinstitute/dsde/workbench/azure/AzureSubscriberSpec.scala
@@ -6,6 +6,7 @@ import cats.effect.{IO, Resource}
 import cats.mtl.Ask
 import com.azure.core.util.BinaryData
 import com.azure.messaging.servicebus.ServiceBusReceivedMessage
+import com.google.protobuf.Timestamp
 import org.broadinstitute.dsde.workbench.model.TraceId
 import org.broadinstitute.dsde.workbench.util2.{ConsoleLogger, LogLevel}
 import org.mockito.Mockito._
@@ -18,9 +19,12 @@ import reactor.core.publisher.Flux
 
 import scala.util.Try
 
-class AzureSubscriberSpec extends AnyFlatSpecLike with MockitoSugar with Matchers with BeforeAndAfterEach  {
-  var mockReceiverClient : AzureServiceBusReceiverClientWrapper = _
-  implicit val logger: ConsoleLogger = new ConsoleLogger("unit_test", LogLevel(enableDebug = false, enableTrace = false, enableInfo = true, enableWarn = true))
+class AzureSubscriberSpec extends AnyFlatSpecLike with MockitoSugar with Matchers with BeforeAndAfterEach {
+  var mockReceiverClient: AzureServiceBusReceiverClientWrapper = _
+  implicit val logger: ConsoleLogger = new ConsoleLogger(
+    "unit_test",
+    LogLevel(enableDebug = false, enableTrace = false, enableInfo = true, enableWarn = true)
+  )
   implicit val traceIdAsk: Ask[IO, TraceId] = Ask.const[IO, TraceId](TraceId("TRACE-ID"))
   override def beforeEach(): Unit = {
     super.beforeEach()
@@ -30,7 +34,7 @@ class AzureSubscriberSpec extends AnyFlatSpecLike with MockitoSugar with Matcher
   // Okay, using the last resort approach to create a received message: reflection
   // This is because ServiceBusReceivedMessage is a final class and cannot be mocked, even when mockito is configured to mock final classes
   // and the SDK does not provide a factory to create a received message
-  def createServiceBusReceivedMessageUsingReflection(body: String) : ServiceBusReceivedMessage = {
+  def createServiceBusReceivedMessageUsingReflection(body: String): ServiceBusReceivedMessage = {
     val clazz = classOf[ServiceBusReceivedMessage]
     val constructor = clazz.getDeclaredConstructor(classOf[BinaryData])
     constructor.setAccessible(true)
@@ -38,29 +42,65 @@ class AzureSubscriberSpec extends AnyFlatSpecLike with MockitoSugar with Matcher
   }
 
   "AzureSubscriber" should "receive a string message successfully" in {
-    //val queue = Queue.unbounded[IO, AzureEvent[String]].unsafeRunSync()
     val receivedMessage = createServiceBusReceivedMessageUsingReflection("test")
 
     // Mock the messages received from the service bus
     val messagesFlux = Flux.just(receivedMessage)
     when(mockReceiverClient.receiveMessagesAsync()).thenReturn(messagesFlux)
 
-    // Create and use the subscriber
+
     val res = for {
       queue <- Queue.unbounded[IO, AzureEvent[String]]
-      _ <- AzureSubscriberInterpreter.stringSubscriber[IO](mockReceiverClient, queue).use { sub =>
-        sub.start
-      }
-      messageOrTimeout <- IO.race(queue.take, IO.sleep(3.seconds))
-    } yield messageOrTimeout
+      _ <- AzureSubscriberInterpreter.stringSubscriber[IO](mockReceiverClient, queue).use (sub =>
+        sub.start)
+      _  <- queue.take
+  } yield ()//IO.race(queue.take, IO.sleep(3.seconds))
+
+    res.unsafeRunSync()
+
+    // queue.offer(AzureEvent("test",None,Timestamp.getDefaultInstance)).unsafeRunSync()
+
+    // val x = queue.take.unsafeRunSync()
+
+    // val res1 =  IO.race(queue.take, IO.sleep(3.seconds)).unsafeRunSync()
+
+//    res match {
+//      case Left(message) => println(s"Received message: $message")
+//      case Right(_)      => println("Timeout reached without receiving a message")
+//    }
 
     // Run the test and verify the results
-    val testResult = Try {
-      res.unsafeRunSync() match {
-        case Left(message) => println(s"Received message: $message")
-        case Right(_)      => println("Timeout reached without receiving a message")
-      }
-    }
-    testResult should be a Symbol("success")
+//    val testResult = Try {
+//      res.unsafeRunSync() match {
+//        case Left(message) => println(s"Received message: $message")
+//        case Right(_)      => println("Timeout reached without receiving a message")
+//      }
+//    }
+    // testResult should be a Symbol("success")
+    // res.unsafeRunSync() should be a Symbol("success")
+  }
+
+  "AzureSubscriber" should "simple dequeue" in {
+
+    val items = Flux.just("hello", "world", "!")
+
+    val queue: Queue[IO, String] = Queue.unbounded[IO, String].unsafeRunSync()
+
+    items.subscribe((item: String) => queue.offer(item).unsafeRunAsync(c => println(s"Completed: $c")),
+                    (error: Throwable) => println(s"Error: $error")
+    )
+
+    val program: IO[Unit] = for {
+
+      // Read the event from the queue
+      dequeuedEvent <- queue.take
+
+      // Do something with the dequeued event
+      _ = println(dequeuedEvent)
+
+    } yield ()
+
+    // Run the program
+    program.unsafeRunSync()
   }
 }
