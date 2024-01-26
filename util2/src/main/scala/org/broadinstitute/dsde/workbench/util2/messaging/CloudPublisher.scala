658704d0c1a3e3614cad12df91f4bfe8f4f6bfbc
Jesus Aguilar <3589801+giventocode@users.noreply.github.com>
2024-01-26 08:07:41 -0500

    format utils2

--- util2/src/main/scala/org/broadinstitute/dsde/workbench/util2/messaging/CloudPublisher.scala
+++ util2/src/main/scala/org/broadinstitute/dsde/workbench/util2/messaging/CloudPublisher.scala
@@ -7,6 +7,8 @@ import org.broadinstitute.dsde.workbench.model.TraceId
 
 trait CloudPublisher[F[_]] {
   def publish[MessageType: Encoder]: Pipe[F, MessageType, Unit]
-  def publishOne[MessageType: Encoder](message: MessageType, messageAttributes: Option[Map[String, String]] = None)(implicit ev: Ask[F, TraceId]): F[Unit]
+  def publishOne[MessageType: Encoder](message: MessageType, messageAttributes: Option[Map[String, String]] = None)(
+    implicit ev: Ask[F, TraceId]
+  ): F[Unit]
   def publishString: Pipe[F, String, Unit]
 }
