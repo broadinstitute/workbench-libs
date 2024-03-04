a53508e3089bdf876735ce1a375637ce15da8e82
Blake Geno <bgeno@broadinstitute.org>
2024-03-04 14:31:02 -0500

    simplify match statement

--- serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/test/CleanUp.scala
+++ serviceTest/src/test/scala/org/broadinstitute/dsde/workbench/service/test/CleanUp.scala
@@ -114,15 +114,7 @@ object CleanUp {
 
   def runCodeWithCleanup[T, C](testTrial: Try[T], cleanupTrial: Try[C]): T =
     (testTrial, cleanupTrial) match {
-      case (Success(outcome), Success(_)) => outcome
-      case (Failure(t), Success(_))       => throw t
-      case (Success(outcome), Failure(_)) => outcome
-      case (Failure(t), Failure(c)) =>
-        throw new WorkbenchExceptionWithErrorReport(
-          ErrorReport(
-            "Test and CleanUp both failed. This ErrorReport's causes contain the test and cleanup exceptions, in that order",
-            Seq(ErrorReport(t), ErrorReport(c))
-          )
-        )
+      case (Success(outcome), _) => outcome
+      case (Failure(t), _)       => throw t
     }
 }
