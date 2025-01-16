15e2fe5abcbb4761f4a9a191e92e36ebfa554838
David An <davidan@broadinstitute.org>
2025-01-16 11:57:28 -0500

    CORE-266: enforce a single reply-to address on group-access-request notifications (#1717)
    * GroupAccessRequestNotificationV2 with a single reply-to
    
    * docs and library versions
    
    * scalafmt

--- notifications/src/main/scala/org/broadinstitute/dsde/workbench/model/Notification.scala
+++ notifications/src/main/scala/org/broadinstitute/dsde/workbench/model/Notification.scala
@@ -183,6 +183,9 @@ object Notifications {
     override val description = "Aborted submission"
   })
 
+  @deprecated(message = "use GroupAccessRequestNotificationV2 instead", since = "2025-01-15")
+  // sendgrid v3 java library does not support multiple reply-tos.
+  // GroupAccessRequestNotificationV2 enforces a single reply-to.
   case class GroupAccessRequestNotification(recipientUserId: WorkbenchUserId,
                                             groupName: String,
                                             replyToIds: Set[WorkbenchUserId],
@@ -190,6 +193,17 @@ object Notifications {
   ) extends Notification
   val GroupAccessRequestNotificationType = register(new NotificationType[GroupAccessRequestNotification] {
     override val format: RootJsonFormat[GroupAccessRequestNotification] = jsonFormat4(GroupAccessRequestNotification)
+    override val description = "Group Access Requested"
+  })
+
+  case class GroupAccessRequestNotificationV2(recipientUserId: WorkbenchUserId,
+                                              groupName: String,
+                                              replyToId: WorkbenchUserId,
+                                              requesterId: WorkbenchUserId
+  ) extends Notification
+  val GroupAccessRequestNotificationTypeV2 = register(new NotificationType[GroupAccessRequestNotificationV2] {
+    override val format: RootJsonFormat[GroupAccessRequestNotificationV2] =
+      jsonFormat4(GroupAccessRequestNotificationV2)
     override val description = "Group Access Requested"
   })
 
