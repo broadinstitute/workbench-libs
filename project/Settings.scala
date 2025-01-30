a226b32d6b1f1331f9bd5197b135464b4128ebe6
Jose Soto <jsoto@broadinstitute.org>
2025-01-30 11:00:59 -0500

    more versioning stuffs

--- project/Settings.scala
+++ project/Settings.scala
@@ -160,7 +160,7 @@ object Settings {
   val notificationsSettings = commonSettings ++ List(
     name := "workbench-notifications",
     libraryDependencies ++= notificationsDependencies,
-    version := createVersion("1.0")
+    version := createVersion("1.1")
   ) ++ publishSettings
 
   val oauth2Settings = commonSettings ++ List(
