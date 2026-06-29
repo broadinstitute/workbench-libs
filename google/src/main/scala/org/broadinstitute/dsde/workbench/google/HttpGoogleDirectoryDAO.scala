9ba70f803d8170d044487fd537bf142d781c94ef
David An <davidan@broadinstitute.org>
2026-06-29 09:04:38 -0400

    scalafmt, again

--- google/src/main/scala/org/broadinstitute/dsde/workbench/google/HttpGoogleDirectoryDAO.scala
+++ google/src/main/scala/org/broadinstitute/dsde/workbench/google/HttpGoogleDirectoryDAO.scala
@@ -62,12 +62,12 @@ class HttpGoogleDirectoryDAO(appName: String,
         .setAllowExternalMembers(allowExternalMembers.toString.toLowerCase)
       val updater = settingsClient.groups().patch(groupEmail.value, content)
       retry(when5xx,
-        whenUsageLimited,
-        when404,
-        whenInvalidValueOnBucketCreation,
-        whenNonHttpIOException,
-        when400,
-        whenGroupDoesNotExist
+            whenUsageLimited,
+            when404,
+            whenInvalidValueOnBucketCreation,
+            whenNonHttpIOException,
+            when400,
+            whenGroupDoesNotExist
       ) { () =>
         executeGoogleRequest(updater)
       }
